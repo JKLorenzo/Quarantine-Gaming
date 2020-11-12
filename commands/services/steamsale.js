d418b5846b6e995ad30f9e8c5118e54e1b78021e
Juruel Keanu Lorenzo <keanulorenzo32@gmail.com>
2020-11-12 19:18:59 +0800

    Add steamsale command to services

---
+++ commands/services/steamsale.js
@@ -0,0 +1,67 @@
+const { Command } = require('discord.js-commando');
+const axios = require('axios');
+const jsdom = require("jsdom");
+const { JSDOM } = jsdom;
+
+module.exports = class SteamSale extends Command {
+    constructor(client) {
+        super(client, {
+            name: 'steamsale',
+            group: 'services',
+            memberName: 'steamsale',
+            description: "Gets the next Steam Sale Day."
+        });
+    }
+
+    async run(message) {
+        await message.say(`Getting information...`).then(async this_message => {
+            let response;
+            await axios.get('https://www.whenisthenextsteamsale.com/').then(resp => {
+                const { document } = (new JSDOM(resp.data)).window;
+                const data = JSON.parse(document.getElementById("hdnNextSale").getAttribute("value"));
+                const info = {
+                    Name: data.Name,
+                    Length: data.Length,
+                    RemainingTime: {
+                        days: data.RemainingTime.split('.')[0],
+                        hours: data.RemainingTime.split('.')[1].split(':')[0],
+                        minutes: data.RemainingTime.split('.')[1].split(':')[1],
+                        seconds: data.RemainingTime.split('.')[1].split(':')[2]
+                    },
+                    confirmed: data.IsConfirmed
+                }
+                response = info;
+            }).catch(error => {
+                g_interface.on_error({
+                    name: 'run => .get(axios)',
+                    location: 'steamsale.js',
+                    error: error
+                });
+            });
+
+            if (response) {
+                await this_message.edit(`${response.Name} will start on ${response.RemainingTime.days} days ${response.RemainingTime.hours} hours ${response.RemainingTime.minutes} minutes ${response.RemainingTime.days} seconds and it will be available for ${response.Length} days! ${response.confirmed ? '' : '*Unconfirmed'}`).catch(error => {
+                    g_interface.on_error({
+                        name: 'run => .edit(else)',
+                        location: 'steamsale.js',
+                        error: error
+                    });
+                });
+            } else {
+                await this_message.edit(`No information is available right now.`).catch(error => {
+                    g_interface.on_error({
+                        name: 'run => .edit(else)',
+                        location: 'steamsale.js',
+                        error: error
+                    });
+                });
+            }
+        }).catch(error => {
+            g_interface.on_error({
+                name: 'run => .say(message)',
+                location: 'steamsale.js',
+                error: error
+            });
+        });
+    }
+};
