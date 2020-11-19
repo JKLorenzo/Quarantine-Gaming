c705671937508302ad81fc643116ff6aea5485f9
Juruel Keanu Lorenzo <keanulorenzo32@gmail.com>
2020-11-19 18:10:27 +0800

    Removed autodeletion of !dedicate post-reply.

--- commands/services/dedicate.js
+++ commands/services/dedicate.js
@@ -75,20 +75,14 @@ module.exports = class DedicateCommand extends Command {
                             break
                     }
                 } else {
-                    message.say(`You must be on a dedicated channel to lock or unlock a voice channel.`).then(this_msg => {
-                        this_msg.delete({ timeout: 60000 }).catch(() => { });
-                    }).catch(() => { });
+                    message.say(`You must be on a dedicated channel to lock or unlock a voice channel.`).catch(() => { });
                 }
             } else {
                 g_channels.dedicate(message.member, name);
-                message.say(`Got it! Please wait.`).then(this_msg => {
-                    this_msg.delete({ timeout: 60000 }).catch(() => { });
-                }).catch(() => { });
+                message.say(`Got it! Please wait.`).catch(() => { });
             }
         } else {
-            message.channel.send(`You must be connected to any voice channels to create a dedicated channel.`).then(this_msg => {
-                this_msg.delete({ timeout: 60000 }).catch(() => { });
-            }).catch(() => { });
+            message.channel.send(`You must be connected to any voice channels to create a dedicated channel.`).catch(() => { });
         }
     }
 };
