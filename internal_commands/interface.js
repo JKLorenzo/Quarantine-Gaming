2add21861bbd0ca19dbf24c6c6bffe76465fc667
Juruel Keanu Lorenzo <keanulorenzo32@gmail.com>
2020-11-20 10:11:59 +0800

    Improved Issue Detection through EPM Thresholding.

--- internal_commands/interface.js
+++ internal_commands/interface.js
@@ -25,51 +25,79 @@ const log = async function (message) {
     });
 }
 
-let error_count = 0;
-const on_error = async function (details) {
-    // Service Unavailable, User aborted request
-    if (error_count == 5) {
 
-        // Change bot presence
-        g_functions.setActivity(`SERVER RESTART`);
-
-        // Announce
-        await announce(`**Discord Status Updates**\nDiscord is currently having some issues and may impact users on this server. Visit <https://discordstatus.com/> for more info.`).catch(async () => {
-            let embed = new MessageEmbed();
-            embed.setAuthor('Limited Functionality');
-            embed.setTitle('Discord Status Updates');
-            embed.setDescription(`Discord is currently having some issues and may impact users on this server. Visit <https://discordstatus.com/> for more info.`);
-            embed.setColor('ffe300');
-            await announce(embed).catch(() => { });
-        });
-
-        // Notify staffs
-        await g_channels.get().staff.send(`I'm currently detecting issues with Discord; some functionalities are disabled. A bot restart is recommended once the issues are resolved.`).catch(async () => {
-            let embed = new MessageEmbed();
-            embed.setAuthor('Limited Functionality');
-            embed.setTitle('Issues with Discord');
-            embed.setDescription(`I'm currently detecting issues with Discord; some functionalities are disabled. A bot restart is recommended once the issues are resolved.`);
-            embed.setColor('ffe300');
-            await g_channels.get().staff.send(embed).catch(() => { });
-        });
-
-        error_count++;
-
-    } else {
-
-        error_count++;
-
-        let embed = new MessageEmbed()
-        embed.setAuthor('Quarantine Gaming: Telemetry');
-        embed.setTitle('Exception Details');
-        embed.addField('Function', details.name);
-        embed.addField('Message', details.error);
-        embed.addField('Location', details.location);
-        embed.addField('Code', details.error.code);
-        embed.setThumbnail('https://mir-s3-cdn-cf.behance.net/project_modules/disp/c9955d46715833.589222657aded.png');
-        embed.setColor('#FF0000');
-        await log(embed);
+let errors = new Array();
+let is_processing_errors = false;
+const on_error = function (details) {
+    errors.push(details);
+    if (!is_processing_errors) {
+        process_errors();
     }
+}
+
+let threshold_reached = false;
+let errors_per_minute = new Array();
+async function process_errors() {
+    is_processing_errors = true;
+
+    while (errors.length > 0) {
+        const details = errors.shift();
+
+        errors_per_minute.push(details);
+        setTimeout(() => {
+            // Remove expired
+            errors_per_minute.shift();
+            // Reset when EPM is below threshold
+            if (errors_per_minute.length == 0) threshold_reached = false;
+        }, 60000);
+
+        const epm = errors_per_minute.length;
+
+        if (epm > 5) {
+            if (!threshold_reached) {
+                // Change bot presence
+                g_functions.setActivity(`SERVER RESTART`);
+
+                // Announce
+                await announce(`**Discord Status Updates**\nDiscord is currently having some issues and may impact users on this server. Visit <https://discordstatus.com/> for more info.`).catch(async () => {
+                    let embed = new MessageEmbed();
+                    embed.setAuthor('Limited Functionality');
+                    embed.setTitle('Discord Status Updates');
+                    embed.setDescription(`Discord is currently having some issues and may impact users on this server. Visit <https://discordstatus.com/> for more info.`);
+                    embed.setColor('ffe300');
+                    await announce(embed).catch(() => { });
+                });
+
+                // Notify staffs
+                await g_channels.get().staff.send(`I'm currently detecting issues with Discord; some functionalities are disabled. A bot restart is recommended once the issues are resolved.`).catch(async () => {
+                    let embed = new MessageEmbed();
+                    embed.setAuthor('Limited Functionality');
+                    embed.setTitle('Issues with Discord');
+                    embed.setDescription(`I'm currently detecting issues with Discord; some functionalities are disabled. A bot restart is recommended once the issues are resolved.`);
+                    embed.setColor('ffe300');
+                    await g_channels.get().staff.send(embed).catch(() => { });
+                });
+
+                threshold_reached = true;
+            } else {
+                // Suppressed Errors
+            }
+        } else {
+            let embed = new MessageEmbed();
+            embed.setAuthor('Quarantine Gaming: Telemetry');
+            embed.setTitle('Exception Details');
+            embed.addField('Function', details.name);
+            embed.addField('Message', details.error);
+            embed.addField('Location', details.location);
+            embed.addField('Code', details.error.code);
+            embed.addField('Errors per Minute', epm);
+            embed.setThumbnail('https://mir-s3-cdn-cf.behance.net/project_modules/disp/c9955d46715833.589222657aded.png');
+            embed.setColor('#FF0000');
+            await log(embed);
+        }
+    }
+
+    is_processing_errors = false;
 }
 
 const updates = async function (message) {
