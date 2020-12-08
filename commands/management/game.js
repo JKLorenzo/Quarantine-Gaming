ee66b376fcd01058719a25c71a269765a3911686
Juruel Keanu Lorenzo <keanulorenzo32@gmail.com>
2020-12-08 22:52:44 +0800

    Updated !game feedback message and logic.

--- commands/management/game.js
+++ commands/management/game.js
@@ -17,7 +17,7 @@ module.exports = class Game extends Command {
                 },
                 {
                     key: 'name',
-                    prompt: `Enter the name of the game.`,
+                    prompt: `Enter the name of the game. Case insensitive.`,
                     type: 'string',
                     validate: name => name.trim().length > 0
                 }
@@ -27,23 +27,29 @@ module.exports = class Game extends Command {
 
     async run(message, { mode, name }) {
         let updated = false;
+        let reply = await message.reply('Updating database...').catch(() => { });;
         switch (mode) {
             case 'whitelist':
                 if (await g_db.pushWhitelisted(name.trim())) {
+                    await g_functions.sleep(2500);
+                    reply.edit('Applying changes...').catch(() => { });;
                     updated = await g_dynamic_roles.init();
                 }
                 break;
             case 'blacklist':
                 if (await g_db.pushBlacklisted(name.trim())) {
+                    await g_functions.sleep(2500);
+                    reply.edit('Applying changes...').catch(() => { });;
                     updated = await g_dynamic_roles.init();
                 };
                 break;
         }
 
+        await g_functions.sleep(2500);
         if (updated) {
-            message.say('Updated!').catch(() => { });;
+            reply.edit(`Updated! ${name} is now ${mode}ed.`).catch(() => { });;
         } else {
-            message.say('No changes made!').catch(() => { });;
+            reply.edit(`No changes made. Failed to ${mode} ${name}.`).catch(() => { });;
         }
     }
 };
