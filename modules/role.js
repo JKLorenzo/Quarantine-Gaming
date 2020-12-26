2e38c109e3bf7f274c7d5ea7798db5ddb3741dad
Juruel Keanu Lorenzo <keanulorenzo32@gmail.com>
2020-12-26 12:24:22 +0800

    Updated role.js create/delete functions.

--- modules/role.js
+++ modules/role.js
@@ -1,5 +1,54 @@
+const app = require('./app.js');
+const functions = require('./functions.js');
 
+const RoleCreateManager = functions.createManager(1000);
+const RoleDeleteManager = functions.createManager(5000);
 
 module.exports = {
-    
+    create: function (options = {
+        name: null,
+        color: null,
+        hoist: null,
+        mentionable: null,
+        permissions: null,
+        position: null,
+        reason: null,
+
+        get: function () {
+            const options;
+            if (this.name !== null) options.data.name = this.name;
+            if (this.color !== null) options.data.color = this.color;
+            if (this.hoist !== null) options.data.hoist = this.hoist;
+            if (this.mentionable !== null) options.data.mentionable = this.mentionable;
+            if (this.permissions !== null) options.data.permissions = this.permissions;
+            if (this.position !== null) options.data.position = this.position;
+            if (this.reason !== null) options.reason = this.reason;
+            return options;
+        }
+    }) {
+        return new Promise(async (resolve, reject) => {
+            await RoleCreateManager.queue();
+            let output, error;
+            try {
+                output = await app.guild.roles.create(options.get());
+            } catch (err) {
+                error = err;
+            }
+            RoleCreateManager.finish();
+            error ? reject(error) : resolve(output)
+        });
+    },
+    delete: function (role) {
+        return new Promise(async (resolve, reject) => {
+            await RoleDeleteManager.queue();
+            let output, error = '';
+            try {
+                output = await app.role(role).delete();
+            } catch (err) {
+                error = err;
+            }
+            RoleDeleteManager.finish();
+            error ? reject(error) : resolve(output)
+        })
+    }
 }
