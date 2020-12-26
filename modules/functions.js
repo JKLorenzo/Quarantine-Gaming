5ee7b6cf8def47c1512dc330d30df1ab24146431
Juruel Keanu Lorenzo <keanulorenzo32@gmail.com>
2020-12-26 12:22:01 +0800

    Updated functions.js for consistency.

--- modules/functions.js
+++ modules/functions.js
@@ -1,53 +1,43 @@
-
-function sleep(ms) {
-    return new Promise(resolve => setTimeout(resolve, ms));
-}
-
-function createStructure(names) {
-    var names = names.split(' ');
-    var count = names.length;
-    function constructor() {
-        for (var i = 0; i < count; i++) {
-            this[names[i]] = arguments[i];
-        }
-    }
-    return constructor;
-}
-
-function createManager(timeout) {
-    const ProcessManager = {
-        processID: 0,
-        currentID: 0,
-
-        queue: function () {
-            const id = this.processID++;
-            return new Promise(async resolve => {
-                while (id != this.currentID) await sleep(1000);
-                resolve();
-            });
-        },
-        finish: function () {
-            setTimeout(() => {
-                this.currentID++;
-            }, timeout);
-        }
-    }
-    return ProcessManager;
-}
-
-function compareDate(date) {
-    const today = new Date();
-    const diffMs = (today - date);
-    return {
-        days: Math.floor(diffMs / 86400000),
-        hours: Math.floor((diffMs % 86400000) / 3600000),
-        minutes: Math.round(((diffMs % 86400000) % 3600000) / 60000)
-    };
-}
-
 module.exports = {
-    sleep,
-    createStructure,
-    createManager,
-    compareDate
+    sleep: function (ms) {
+        return new Promise(resolve => setTimeout(resolve, ms));
+    },
+    createStructure: function (properties) {
+        const property = properties.split(' ');
+        const count = properties.length;
+        function constructor() {
+            for (let i = 0; i < count; i++) {
+                this[property[i]] = arguments[i];
+            }
+        }
+        return constructor;
+    },
+    createManager: function (timeout) {
+        return {
+            processID: 0,
+            currentID: 0,
+
+            queue: function () {
+                const id = this.processID++;
+                return new Promise(async resolve => {
+                    while (id != this.currentID) await sleep(1000);
+                    resolve();
+                });
+            },
+            finish: function () {
+                setTimeout(() => {
+                    this.currentID++;
+                }, timeout);
+            }
+        };
+    },
+    compareDate: function (date) {
+        const today = new Date();
+        const diffMs = (today - date);
+        return {
+            days: Math.floor(diffMs / 86400000),
+            hours: Math.floor((diffMs % 86400000) / 3600000),
+            minutes: Math.round(((diffMs % 86400000) % 3600000) / 60000)
+        };
+    }
 }
