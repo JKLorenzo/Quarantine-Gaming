74fde7d7ecd1bd8bb41b9578b12d11cd37303d1a
Juruel Keanu Lorenzo <keanulorenzo32@gmail.com>
2020-12-30 17:02:21 +0800

    Imported functions array_difference to compareArray

--- modules/functions.js
+++ modules/functions.js
@@ -40,6 +40,23 @@ module.exports = {
             minutes: Math.round(((diffMs % 86400000) % 3600000) / 60000)
         };
     },
+    compareArray: function (array1, array2) {
+        let a = [], difference = [];
+        for (let i = 0; i < array1.length; i++) {
+            a[array1[i]] = true;
+        }
+        for (let i = 0; i < array2.length; i++) {
+            if (a[array2[i]]) {
+                delete a[array2[i]];
+            } else {
+                a[array2[i]] = true;
+            }
+        }
+        for (let k in a) {
+            difference.push(k);
+        }
+        return difference;
+    },
     toAlphanumericString: function (string) {
         return String(string).replace(/[^a-z0-9]/gi, '')
     }
