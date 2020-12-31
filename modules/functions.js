0a0dc0ac55b53a9625a8c6140bcd26706bcce44e
Juruel Keanu Lorenzo <keanulorenzo32@gmail.com>
2020-12-31 10:53:24 +0800

    Import parseHTML, contains, and fetchIcon to functions

--- modules/functions.js
+++ modules/functions.js
@@ -59,5 +59,34 @@ module.exports = {
     },
     toAlphanumericString: function (string) {
         return String(string).replace(/[^a-z0-9]/gi, '')
+    },
+    parseHTML: function (string) {
+        return String(string).replace('&amp;', '&').replace('&lt;', '<').replace('&gt;', '>').replace('&quot;', '"');
+    },
+    contains: function (whole_string, part_string) {
+        return String(whole_string).toLowerCase().indexOf(String(part_string).toLowerCase()) !== -1;
+    },
+    fetchIcon: function (hostname) {
+        if (this.contains(hostname, 'reddit')) {
+            return 'https://image.flaticon.com/icons/png/512/355/355990.png';
+        } else if (this.contains(hostname, 'steam')) {
+            return 'https://upload.wikimedia.org/wikipedia/commons/thumb/8/83/Steam_icon_logo.svg/1024px-Steam_icon_logo.svg.png';
+        } else if (this.contains(hostname, 'epicgames')) {
+            return 'https://cdn2.unrealengine.com/EpicGames%2Fno-exist-576x576-5c7c5c6c4edc402cbd0d369cf7dd2662206b4657.png';
+        } else if (this.contains(hostname, 'gog')) {
+            return 'https://static.techspot.com/images2/downloads/topdownload/2016/12/gog.png';
+        } else if (this.contains(hostname, 'playstation')) {
+            return 'https://lh3.ggpht.com/pYDuCWSs7TIopjHX_i89et1C6zyk82iRZKAiWe8yJt5KNXp-B2ZuK7KHydkpaQmAnV0=w300';
+        } else if (this.contains(hostname, 'xbox')) {
+            return 'https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/i/0428cd5e-b1ca-4c7c-8d6a-0b263465bfe0/d4hcb91-d614c470-8051-43ef-ab75-18100a527bd1.png';
+        } else if (this.contains(hostname, 'ubisoft')) {
+            return 'https://vignette.wikia.nocookie.net/ichc-channel/images/e/e2/Ubisoft_round_icon_by_slamiticon-d66j9vs.png/revision/latest/scale-to-width-down/220?cb=20160328232011';
+        } else if (this.contains(hostname, 'microsoft')) {
+            return 'https://cdn0.iconfinder.com/data/icons/shift-free/32/Microsoft-512.png';
+        } else if (this.contains(hostname, 'discord')) {
+            return 'https://i1.pngguru.com/preview/373/977/320/discord-for-macos-white-and-blue-logo-art.jpg';
+        } else {
+            return `http://www.google.com/s2/favicons?domain=${hostname}`;
+        }
     }
 }
