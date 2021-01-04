dc1bb5f1f5238149896293464f8b4fbcef2a98e9
Juruel Keanu Lorenzo <keanulorenzo32@gmail.com>
2021-01-04 21:15:16 +0800

    Refactor !checkrole management command.

--- commands/management/checkrole.js
+++ commands/management/checkrole.js
@@ -1,5 +1,8 @@
 const { Command } = require('discord.js-commando');
 const { MessageEmbed } = require('discord.js');
+const functions = require('../../modules/functions.js');
+const constants = require('../../modules/constants.js');
+let app = require('../../modules/app.js');
 
 module.exports = class CheckRole extends Command {
     constructor(client) {
@@ -7,68 +10,83 @@ module.exports = class CheckRole extends Command {
             name: 'checkrole',
             group: 'management',
             memberName: 'checkrole',
-            description: '[Admin Only] Gets the permissions of a role in the target channel.',
+            description: '[Admin Only] Gets the permissions of a member or a role in a channel.',
             guildOnly: true,
             args: [
                 {
-                    key: 'role',
-                    prompt: 'Mention the role you want to check.',
-                    type: 'role'
+                    key: 'MemberOrRole',
+                    prompt: 'Mention the member/ID or role/ID you want to check.',
+                    type: 'string',
+                    validate: MemberOrRole => {
+                        // Link
+                        const Modules = functions.parseModules(GlobalModules);
+                        app = Modules.app;
+
+                        MemberOrRole = functions.parseMention(MemberOrRole);
+                        if (app.member(MemberOrRole) || app.role(MemberOrRole))
+                            return true;
+                        return false;
+                    }
                 },
                 {
-                    key: 'channel',
-                    prompt: 'Mention the channel.',
-                    type: 'channel'
+                    key: 'Channel',
+                    prompt: 'Mention the target channel/ID.',
+                    type: 'string',
+                    validate: Channel => {
+                        Channel = functions.parseMention(Channel);
+                        if (app.channel(Channel))
+                            return true;
+                        return false;
+                    }
                 }
             ]
         });
     }
 
-    async run(message, { role, channel }) {
-        message.delete({ timeout: 60000 }).catch(() => { });
+    async run(message, { MemberOrRole, Channel }) {
+        // Parse Mentions
+        MemberOrRole = functions.parseMention(MemberOrRole);
+        const this_object = app.member(MemberOrRole) || app.role(MemberOrRole);
+        const this_channel = app.channel(functions.parseMention(Channel));
 
-        let permissions = g_channels.get().guild.channels.cache.get(channel.id).permissionsFor(role);
-        const generalPermissions = [
-            'CREATE_INSTANT_INVITE', 'MANAGE_CHANNELS', 'MANAGE_ROLES', 'MANAGE_WEBHOOKS'
-        ]
-        const textPermissions = [
-            'VIEW_CHANNEL', 'SEND_MESSAGES', 'SEND_TTS_MESSAGES', 'MANAGE_MESSAGES', 'EMBED_LINKS', 'ATTACH_FILES', 'READ_MESSAGE_HISTORY', 'MENTION_EVERYONE', 'USE_EXTERNAL_EMOJIS', 'ADD_REACTIONS'
-        ]
-        const voicePermissions = [
-            'VIEW_CHANNEL', 'CONNECT', 'SPEAK', 'STREAM', 'MUTE_MEMBERS', 'DEAFEN_MEMBERS', 'MOVE_MEMBERS', 'PRIORITY_SPEAKER'
-        ]
-
-        let output = new Array();
-        for (let this_permission of generalPermissions) {
-            output.push({
-                name: this_permission.split('_').map(text => text.substring(0, 1).toUpperCase() + text.slice(1).toLowerCase()).join(' '),
-                value: permissions.has(this_permission)
+        const object_permissions = this_channel.permissionsFor(this_object);
+        const embed = new MessageEmbed();
+        embed.setAuthor('Quarantine Gaming: Bitwise Permission Flags');
+        embed.setTitle('Channel Permissions')
+        embed.setDescription(`${this_object} permissions on ${this_channel} channel.`);
+        // General Permissions
+        let permissions = new Array();
+        for (const this_permission of Object.entries(constants.permissions.general)) {
+            permissions.push({
+                name: this_permission[0].split('_').map(text => text.substring(0, 1).toUpperCase() + text.slice(1).toLowerCase()).join(' '),
+                value: object_permissions.has(this_permission[1])
             });
         }
-        switch (channel.type) {
+        embed.addField('General Category Permissions', permissions.map(permission => `${permission.name}: ${permission.value ? '✅' : '❌'}`).join('\n'));
+        // Type-Specific Permissions
+        permissions = new Array();
+        switch (this_channel.type) {
             case 'text':
-                for (let this_permission of textPermissions) {
-                    output.push({
-                        name: this_permission.split('_').map(text => text.substring(0, 1).toUpperCase() + text.slice(1).toLowerCase()).join(' '),
-                        value: permissions.has(this_permission)
+                for (const this_permission of Object.entries(constants.permissions.text)) {
+                    permissions.push({
+                        name: this_permission[0].split('_').map(text => text.substring(0, 1).toUpperCase() + text.slice(1).toLowerCase()).join(' '),
+                        value: object_permissions.has(this_permission[1])
                     });
                 }
+                embed.addField('Text Channel Permissions', permissions.map(permission => `${permission.name}: ${permission.value ? '✅' : '❌'}`).join('\n'));
                 break;
             case 'voice':
-                for (let this_permission of voicePermissions) {
-                    output.push({
-                        name: this_permission.split('_').map(text => text.substring(0, 1).toUpperCase() + text.slice(1).toLowerCase()).join(' '),
-                        value: permissions.has(this_permission)
+                for (const this_permission of Object.entries(constants.permissions.voice)) {
+                    permissions.push({
+                        name: this_permission[0].split('_').map(text => text.substring(0, 1).toUpperCase() + text.slice(1).toLowerCase()).join(' '),
+                        value: object_permissions.has(this_permission[1])
                     });
                 }
+                embed.addField('Voice Channel Permissions', permissions.map(permission => `${permission.name}: ${permission.value ? '✅' : '❌'}`).join('\n'));
                 break;
         }
-        let embed = new MessageEmbed();
-        embed.setTitle(`**${role.name} Role**`);
-        embed.setDescription(output.map(data => `${data.name}: ${data.value ? '✅' : '❌'}`));
-        embed.setAuthor('Quarantine Gaming: Role Information', g_client.user.displayAvatarURL());
-        embed.setFooter(`On ${channel.name} channel`);
-        embed.setTimestamp(new Date());
-        return message.say(embed).catch(() => { });;
+        embed.setTimestamp();
+        embed.setColor('#ffff00');
+        message.say(embed);
     }
 };
