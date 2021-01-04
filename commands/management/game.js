2142dc68e67e4d1b223dc8e98038cfe27a635eb6
Juruel Keanu Lorenzo <keanulorenzo32@gmail.com>
2021-01-04 21:15:30 +0800

    Refactor !game management command.

--- commands/management/game.js
+++ commands/management/game.js
@@ -1,4 +1,8 @@
 const { Command } = require('discord.js-commando');
+const functions = require('../../modules/functions.js');
+const constants = require('../../modules/constants.js');
+let app = require('../../modules/app.js');
+let database = require('../../modules/database.js');
 
 module.exports = class Game extends Command {
     constructor(client) {
@@ -17,7 +21,7 @@ module.exports = class Game extends Command {
                 },
                 {
                     key: 'name',
-                    prompt: `Enter the name of the game. Case insensitive.`,
+                    prompt: `Enter the name of the game. (Case insensitive)`,
                     type: 'string',
                     validate: name => name.trim().length > 0
                 }
@@ -26,44 +30,53 @@ module.exports = class Game extends Command {
     }
 
     async run(message, { mode, name }) {
-        let updated = false;
+        // Link
+        const Modules = functions.parseModules(GlobalModules);
+        app = Modules.app;
+        database = Modules.database;
+
         // Check if anyone is playing this game name
-        let reply = await message.reply('Checking for players...').catch(() => { });;
-        let has_players = false
-        // Check roles
-        for (let this_role of g_channels.get().guild.roles.cache.array()) {
-            if (this_role.name == name.trim()) {
-                has_players = true;
+        name = name.trim().toLowerCase();
+        let updated = false;
+        const reply = await message.reply('Checking for players...').catch(() => { });;
+        let game_name = '';
+        // Check Roles
+        for (const this_role of app.guild().roles.cache.array()) {
+            if (functions.contains(this_role.name.trim().toLowerCase(), name)) {
+                game_name = this_role.name.trim();
             }
         }
-        if (has_players) {
-            // Check presence
-            for (let this_member of g_channels.get().guild.members.cache.array()) {
-                if (this_member.presence.activities.map(activity => activity.name.trim().toLowerCase()).includes(name.trim().toLowerCase())) {
-                    has_players = true;
+        // Check Presence
+        for (const this_member of app.guild().members.cache.array()) {
+            for (const this_activity of this_member.presence.activities) {
+                if (functions.compareString(this_activity.name.trim().toLowerCase(), name) >= 75) {
+                    game_name = this_activity.name.trim();
                 }
             }
         }
 
-        await g_functions.sleep(2500);
+        await functions.sleep(2500);
 
-        if (has_players) {
-            reply.edit('Updating databases...').catch(() => { });;
-            if ((mode == 'whitelist' && await g_db.pushWhitelisted(name.trim())) || (mode == 'blacklist' && await g_db.pushBlacklisted(name.trim()))) {
-                await g_functions.sleep(2500);
-                reply.edit('Applying changes...').catch(() => { });;
-                updated = await g_dynamic_roles.init();
+        if (game_name) {
+            await reply.edit(`Game title matched: ${game_name}. Updating databases...`).catch(() => { });;
+            switch (mode) {
+                case 'whitelist':
+                    updated = await database.gameWhitelist(game_name);
+                    break;
+                case 'blacklist':
+                    updated = await database.gameBlacklist(game_name);
+                    break;
             }
 
-            await g_functions.sleep(2500);
+            await functions.sleep(2500);
 
             if (updated) {
-                reply.edit(`Updated! ${name} is now ${mode}ed.`).catch(() => { });;
+                await reply.edit(`Changes made! ${game_name} is now ${mode}ed.`).catch(() => { });;
             } else {
-                reply.edit(`No changes made. Failed to ${mode} ${name} while updating.`).catch(() => { });;
+                await reply.edit(`No changes made. Failed to ${mode} ${game_name} while updating my database.`).catch(() => { });;
             }
         } else {
-            reply.edit(`No changes made. Failed to ${mode} ${name}. No active players found.`).catch(() => { });;
+            await reply.edit(`No changes made. No match found for ${name}. `).catch(() => { });;
         }
     }
 };
