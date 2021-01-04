f4718b7887315bdc6a7b476939ed3867aeebdc7f
Juruel Keanu Lorenzo <keanulorenzo32@gmail.com>
2021-01-04 21:15:24 +0800

    Refactor !cleanup management command.

--- commands/management/cleanup.js
+++ commands/management/cleanup.js
@@ -1,4 +1,7 @@
 const { Command } = require('discord.js-commando');
+const { MessageEmbed } = require('discord.js');
+const functions = require('../../modules/functions.js');
+let app = require('../../modules/app.js');
 
 module.exports = class CleanUp extends Command {
 	constructor(client) {
@@ -21,43 +24,71 @@ module.exports = class CleanUp extends Command {
 	}
 
 	async run(message, { count }) {
+		// Link
+		const Modules = functions.parseModules(GlobalModules);
+		app = Modules.app;
+
+		await functions.sleep(1000);
 		await message.delete().catch(() => { });
-
-		let remaining = count;
-		let deleted = 0;
-		await g_functions.sleep(1000);
-
+		await functions.sleep(1000);
+		let deleted_messages_count = 0;
+		const deleted_messages = new Array();
 		async function removeMessages(number_of_messages) {
 			if (number_of_messages > 0) {
-				await message.channel.bulkDelete(number_of_messages).then(messages => {
-					deleted += messages.size
-					remaining -= deleted;
-					number_of_messages -= deleted;
+				await message.channel.bulkDelete(number_of_messages).then(messages => messages.array()).then(messages => {
+					number_of_messages -= messages.length;
+					count -= messages.length;
+					deleted_messages_count += messages.length;
+					for (const this_message of messages) {
+						if (deleted_messages[`${this_message.author.id}`]) {
+							deleted_messages[`${this_message.author.id}`] += 1;
+						} else {
+							deleted_messages[`${this_message.author.id}`] = 1;
+						}
+					}
 				}).catch(() => { });
 			}
-			if (number_of_messages > 0) {
-				await message.channel.messages.fetch({ limit: 1 }).then(async messages => {
-					if (messages.size > 0) {
-						for (let this_message of messages) {
-							await this_message[1].delete().then(() => {
-								remaining--;
-								deleted++;
+			while (count > 0 && number_of_messages > 0) {
+				await message.channel.messages.fetch({ limit: number_of_messages }).then(messages => messages.array()).then(async messages => {
+					if (messages.length > 0) {
+						for (const this_message of messages) {
+							await this_message.delete().then(async () => {
+								count--;
+								deleted_messages_count++;
+								if (deleted_messages[`${this_message.author.id}`]) {
+									deleted_messages[`${this_message.author.id}`] += 1;
+								} else {
+									deleted_messages[`${this_message.author.id}`] = 1;
+								}
+								await functions.sleep(2000); // Rate Limit
 							}).catch(() => { });
 						}
 					} else {
-						remaining = 0;
+						count = 0;
 					}
+				}).catch(() => {
+					count = 0;
 				});
 			}
 		}
 
-		while (remaining > 100) {
+		while (count > 100) {
 			await removeMessages(100);
+			await functions.sleep(3500); // Rate Limit
 		}
-		await removeMessages(remaining);
+		await removeMessages(count);
 
-		return message.say(`Cleanup complete. ${deleted} messages removed.`).then(this_message => {
-			this_message.delete({ timeout: 5000 }).catch(() => { });
+		const embed = new MessageEmbed();
+		embed.setAuthor('Quarantine Gaming: Cleanup Manager');
+		embed.setTitle('Cleanup Process Complete');
+		embed.setDescription(Object.entries(deleted_messages).map(entry => {
+			return `${app.member(entry[0])} : ${entry[1]}`;
+		}).join('\n'));
+		embed.setFooter(`A total of ${deleted_messages_count} messages were removed.`);
+		embed.setColor('#ffff00');
+
+		message.say(embed).then(this_message => {
+			this_message.delete({ timeout: 60000 }).catch(() => { });
 		});
 	}
 };
