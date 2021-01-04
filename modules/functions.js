ba4a0cbbce320ceac5350b7e1ff8d367f7e4e7b6
Juruel Keanu Lorenzo <keanulorenzo32@gmail.com>
2021-01-04 21:26:15 +0800

    Update compareString() and fix Const TypeError.

--- modules/functions.js
+++ modules/functions.js
@@ -64,29 +64,26 @@ module.exports = {
         return difference;
     },
     compareString: function (string1, string2) {
-        let longer = string1;
-        let shorter = string2;
+        let longer_string = string1;
+        let shorter_string = string2;
 
         if (string1.length < string2.length) {
-            longer = string2;
-            shorter = string1;
+            longer_string = string2;
+            shorter_string = string1;
         }
 
-        const longerLength = longer.length;
-        if (longerLength == 0) {
-            return 100;
-        }
+        if (longer_string.length == 0 || shorter_string.length) return 0;
 
         const costs = new Array();
-        for (const i = 0; i <= longer.length; i++) {
+        for (let i = 0; i <= longer_string.length; i++) {
             let lastValue = i;
-            for (const j = 0; j <= shorter.length; j++) {
+            for (let j = 0; j <= shorter_string.length; j++) {
                 if (i == 0)
                     costs[j] = j;
                 else {
                     if (j > 0) {
                         let newValue = costs[j - 1];
-                        if (longer.charAt(i - 1) != shorter.charAt(j - 1)) {
+                        if (longer_string.charAt(i - 1) != shorter_string.charAt(j - 1)) {
                             newValue = Math.min(Math.min(newValue, lastValue), costs[j]) + 1;
                         }
                         costs[j - 1] = lastValue;
@@ -94,10 +91,10 @@ module.exports = {
                     }
                 }
             }
-            if (i > 0) costs[shorter.length] = lastValue;
+            if (i > 0) costs[shorter_string.length] = lastValue;
         }
 
-        return 100 * ((longerLength - costs[shorter.length]) / longerLength);
+        return 100 * ((longer_string.length - costs[shorter_string.length]) / longer_string.length);
     },
     toAlphanumericString: function (string) {
         return String(string).replace(/[^a-z0-9]/gi, '')
