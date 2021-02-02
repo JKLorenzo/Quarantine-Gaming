dba4d421572a0d16a1e46a39d21cdbe2242126cc
Juruel Keanu Lorenzo <keanulorenzo32@gmail.com>
2021-02-02 13:44:08 +0800

    Conceal, Replace, and Update Firebase Credentials.

--- modules/database.js
+++ modules/database.js
@@ -7,13 +7,9 @@ let error_manager;
 const ErrorTicketManager = new classes.ErrorTicketManager('database.js');
 
 /**
- * @type {{notifications: Firebase.firestore.CollectionReference, titles_blacklisted: Firebase.firestore.CollectionReference, titles_whitelisted: Firebase.firestore.CollectionReference}}
+ * @type {{FreeGames: Firebase.firestore.CollectionReference, GameOverrides: Firebase.firestore.CollectionReference}}
  */
-let DB = {
-    notifications: null,
-    titles_blacklisted: null,
-    titles_whitelisted: null
-};
+let DB;
 
 let index = 0;
 /** @type {Array<classes.Notification>} */
@@ -40,50 +36,47 @@ module.exports.initialize = async (ModulesFunction) => {
 
         Firebase.initializeApp({
             credential: Firebase.credential.cert({
-                "type": 'service_account',
-                "project_id": 'fg-updates-bot',
-                "private_key_id": '0824c03f15a9b7a42e192200f7fbfeec4e14fe99',
-                "private_key": '-----BEGIN PRIVATE KEY-----\nMIIEvAIBADANBgkqhkiG9w0BAQEFAASCBKYwggSiAgEAAoIBAQCRTiFNPLZWyXhr\nZGGpHQRks3v3prPIzCzsbOBv5lAym4e+n4RKwGwy9KHJrlg/wgv2OLu8ggJk2el+\nPsmEH+y+WlSnHBODrvlZ9eXJb15XW+e7Sb3DUTehepQ223VcLO74493BStEMUgbc\nQd/MyJsmnhqcsIjsHMw1HDjOKRoFD300MGQauS+5ZIPcTRLTtkIqNVAiME+ICgHE\nRkPlAxKHgWue8dwzDF9VPgLCR2ZpGOF3tZ5VZPRi6VTElxFDsYyA2NwuEyg7Frpq\n0XDBkK0wqRgK3zfmQZzgD/IRUGLHb74HsaNhbu1XPWBsMNai//gKME25P5j/Ciq4\n11SIwyy3AgMBAAECggEAIU938uzoIB0vre7lNI+iYjODR9K/hFjKM6kCCqUR3Ygq\nJlkLStex9jx+mm2NbZBejaOT5jMnuVb7YCWkrNVkwH6UyXp2Psnt/+GPPA574ir/\nhL/y8MO9rRccwzasQOVMI2KZg6ZTJi/nwraXR6r1ZnT4RNNzkC1J4yMFIr3paR87\ndP1EoyGEaZliIz0HDH3d5tmxIoPYN4rf93GJQNhcOsmTei1h4dwQJIqydHVW86Y8\nC6EJUrhTJYW4i4k4KnAwaCyl/yS9zeMhgaHYys29pBaEvWjpf7DipsosC9taj6Ea\n6Sp87x60LUpkuZQW2i4Pq8cF5yPuadtAfQalKWgkgQKBgQDDTSGXueWjgl3KOKRt\nsqYiN5wZowY9SPVUzXhOEKkb7ydHLYF5ALgKvub3wTua3t1DthxPMFFOXgvrd46G\nlJc/sb4tvn6J1bnAeb8M+Q6zMu3QEteaIEtbl4ilqk6xOc/XvAS3ZLCmsmEQ6hkN\niZYkT1Gf4gpvLS7H5DUM4PQxjQKBgQC+dyFyIXiiG80ACRduKxEJyMGy4X/EWkqH\nTTPphULW6pZsWoX+EEkeUtDKIebLqNDAE3AOvXL4HhGERRjWI+s5CX9QLWvGs1HP\nDrs+N6EvZUKgx4qiQuwgtaKDXvlnwX+MsqNGnAil3DqbO1TY8Pr4o9rVZ0L5Dn0m\n+8VpdfaMUwKBgF66jsy1UnlYxn1LtBWxTXvTVVfqByC6vqR/dRcIZb4y5e5UWDSE\n8L/lkMojY/Hen5w0PM78NLO6UjXIK82DTUmLwR8XAvGARTTi2JRGSacJ0OfX+9O6\nTlMC0Tjpvnmf/Pw7Kl557GUuqH43zicO0VCTWJggX8dFNyelvUWd51QRAoGAErFe\ndvUSAdb4p6g8xHM1mOA7InM/NuYlqmHJVoHdrXoYiUnZHLY8dt1p4GyzWgmXc0J1\nHP06618IGRMu/NVJoK9t71CF41p2DPxFJDYRe7VUdLMgyAwJYKxy5rHLiINVIVnm\n7Gcj8iNLHTEVgrUj7IMoVwlraUYiwlCzb0ZEjEECgYBgIkcGR4mTltbQUob/EbZq\nXa+oPcy3q9ZaAes8yzPSuCLDw56eOuUuKi2jMnJsVwC+9zH2LPQjkHuzBienyD9H\nOGSsoqEmxwr5O16djfjk5EE2GZMvuPwf3kOuS/QR3+PeNfCjGbaksb4D9hWxiQIJ\n4NytO5rR6XWNNqIJ0C8SZQ==\n-----END PRIVATE KEY-----\n',
-                "client_email": 'firebase-adminsdk-rxhqf@fg-updates-bot.iam.gserviceaccount.com',
-                "client_id": '116447630978026448805',
-                "auth_uri": 'https://accounts.google.com/o/oauth2/auth',
-                "token_uri": 'https://oauth2.googleapis.com/token',
-                "auth_provider_x509_cert_url": 'https://www.googleapis.com/oauth2/v1/certs',
-                "client_x509_cert_url": 'https://www.googleapis.com/robot/v1/metadata/x509/firebase-adminsdk-rxhqf%40fg-updates-bot.iam.gserviceaccount.com'
+                type: process.env.FB_type,
+                project_id: process.env.FB_project_id,
+                private_key_id: process.env.FB_private_key_id,
+                private_key: String(process.env.FB_private_key).replace(/\\n/g, '\n'),
+                client_email: process.env.FB_client_email,
+                client_id: process.env.FB_client_id,
+                auth_uri: process.env.FB_auth_uri,
+                token_uri: process.env.FB_token_uri,
+                auth_provider_x509_cert_url: process.env.FB_auth_provider_x509_cert_url,
+                client_x509_cert_url: process.env.FB_client_x509_cert_url
             })
         });
 
         const Firestore = Firebase.firestore();
 
         DB = {
-            notifications: Firestore.collection('notifications'),
-            titles_blacklisted: Firestore.collection('t_blacklisted'),
-            titles_whitelisted: Firestore.collection('t_whitelisted')
+            FreeGames: Firestore.collection('FreeGames'),
+            GameOverrides: Firestore.collection('GameOverrides'),
         };
 
-        const firebase_notifications = await DB.notifications.orderBy('index').get();
-        for (let firebase_notification of firebase_notifications.docs) {
-            const data = firebase_notification.data();
+        const FreeGames = await DB.FreeGames.orderBy('index').get();
+        for (const FreeGame of FreeGames.docs) {
+            const data = FreeGame.data();
             if (data.index > index) index = data.index;
-            notifications.push({
-                id: firebase_notification.id,
-                title: data.title,
-                url: data.url,
-                author: data.author,
-                permalink: data.permalink
-            });
+            notifications.push(new classes.Notification(data.title, data.url, data.author, data.permalink, {
+                id: FreeGame.id
+            }));
         }
 
         await this.notificationTrim();
 
-        const firebase_blacklisted = await DB.titles_blacklisted.get();
-        for (let title of firebase_blacklisted.docs) {
-            blacklisted.push(title.id);
-        }
-
-        const firebase_whitelisted = await DB.titles_whitelisted.get();
-        for (const title of firebase_whitelisted.docs) {
-            whitelisted.push(title.id);
+        const GameOverrides = await DB.GameOverrides.get();
+        for (const override of GameOverrides.docs) {
+            switch (override.data().category) {
+                case 'whitelist':
+                    whitelisted.push(override.id);
+                    break;
+                case 'blacklist':
+                    blacklisted.push(override.id);
+                    break;
+            }
         }
     } catch (error) {
         error_manager.mark(ErrorTicketManager.create('initialize', error))
@@ -97,7 +90,7 @@ module.exports.notificationTrim = async () => {
     while (notifications.length > 5) {
         try {
             const expired_notif = notifications.shift();
-            await DB.notifications.doc(expired_notif.id).delete();
+            await DB.FreeGames.doc(expired_notif.id).delete();
         } catch (error) {
             error_manager.mark(ErrorTicketManager.create('notificationTrim', error));
         }
@@ -139,7 +132,7 @@ module.exports.notificationRecords = (notification) => {
                 return this_notification;
             }
         }
-        return false;
+        return null;
     } catch (error) {
         error_manager.mark(ErrorTicketManager.create('notificationRecords', error));
     }
@@ -166,16 +159,13 @@ module.exports.gameBlacklist = async (title) => {
         let updated = false;
         if (!blacklisted.includes(title)) {
             blacklisted.push(title);
-            await DB.titles_blacklisted.doc(title).set({});
+            await DB.GameOverrides.doc(title).set({
+                category: 'blacklist'
+            });
             updated = true;
         }
-
-        let index = whitelisted.indexOf(title);
-        if (index + 1) {
-            whitelisted.splice(index, 1);
-            await DB.titles_whitelisted.doc(title).delete();
-            updated = true;
-        }
+        const index = whitelisted.indexOf(title);
+        if (index + 1) whitelisted.splice(index, 1);
         return updated;
     } catch (error) {
         error_manager.mark(ErrorTicketManager.create('gameBlacklist', error));
@@ -192,16 +182,13 @@ module.exports.gameWhitelist = async (title) => {
         let updated = false;
         if (!whitelisted.includes(title)) {
             whitelisted.push(title);
-            await DB.titles_whitelisted.doc(title).set({});
+            await DB.GameOverrides.doc(title).set({
+                category: 'whitelist'
+            });
             updated = true;
         }
-
-        let index = blacklisted.indexOf(title);
-        if (index + 1) {
-            blacklisted.splice(index, 1);
-            await DB.titles_blacklisted.doc(title).delete();
-            updated = true;
-        }
+        const index = blacklisted.indexOf(title);
+        if (index + 1) blacklisted.splice(index, 1);
         return updated;
     } catch (error) {
         error_manager.mark(ErrorTicketManager.create('gameWhitelist', error));
