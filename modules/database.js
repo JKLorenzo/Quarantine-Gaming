6b80d2cb901fca1181d550bf5e2e33db7a49c5a9
Juruel Keanu Lorenzo <39931559+JKLorenzo@users.noreply.github.com>
2021-02-09 14:33:28 +0800

    Moved firebase credentials to one env variable.

--- modules/database.js
+++ modules/database.js
@@ -38,18 +38,7 @@ module.exports.initialize = async (ClientInstance) => {
         whitelisted = new Array();
 
         Firebase.initializeApp({
-            credential: Firebase.credential.cert({
-                type: process.env.FB_type,
-                project_id: process.env.FB_project_id,
-                private_key_id: process.env.FB_private_key_id,
-                private_key: String(process.env.FB_private_key).replace(/\\n/g, '\n'),
-                client_email: process.env.FB_client_email,
-                client_id: process.env.FB_client_id,
-                auth_uri: process.env.FB_auth_uri,
-                token_uri: process.env.FB_token_uri,
-                auth_provider_x509_cert_url: process.env.FB_auth_provider_x509_cert_url,
-                client_x509_cert_url: process.env.FB_client_x509_cert_url
-            })
+            credential: Firebase.credential.cert(JSON.parse(process.env.FIREBASE_CREDENTIALS.replace(/'/g, '"').replace(/\n/g, '\\n')))
         });
 
         const Firestore = Firebase.firestore();
