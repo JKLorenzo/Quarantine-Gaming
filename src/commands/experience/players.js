7e6d7a36183a1793a843ffc156391c870c5e06c1
Juruel Keanu Lorenzo <39931559+JKLorenzo@users.noreply.github.com>
2021-02-12 01:27:16 +0800

    Improved !players command structure.

--- src/commands/experience/players.js
+++ src/commands/experience/players.js
@@ -53,21 +53,19 @@ module.exports = class PlayersCommand extends Commando.Command {
 		const in_game = new Array();
 		const online = new Array();
 		const unavailable = new Array();
+		const offline = new Array();
 
 		if (game_role_mentionable) {
 			const game_role = app.guild().roles.cache.find(this_role => game_role_mentionable.name.startsWith(this_role.name) && this_role.hexColor == '#00ffff');
 			if (game_role) {
-				for (const member of app.guild().members.cache.array()) {
-					if (member.roles.cache.has(game_role.id)) {
-						players.push(member);
-						alphabetical.push(member.displayName);
-					}
+				for (const member of game_role.members.array()) {
+					players.push(member);
+					alphabetical.push(member.displayName);
 				}
 
 				const embed = new Discord.MessageEmbed();
 				embed.setAuthor('Quarantine Gaming: List of Players');
 				embed.setTitle(game_role.name);
-				embed.setDescription('All members who have played this game recently are as follows:');
 
 				const emoji = app.guild().emojis.cache.find(this_role => this_role.name == game_role.name.trim().split(' ').join('').split(':').join('').split('-').join(''));
 				const qg_emoji = app.guild().emojis.cache.find(this_role => this_role.name == 'quarantinegaming');
@@ -81,19 +79,27 @@ module.exports = class PlayersCommand extends Commando.Command {
 							if (this_player.roles.cache.find(this_role => this_role.name == `Play ${game_role.name}`)) {
 								in_game.push(this_player);
 							}
-							else if (this_player.presence.status == 'online') {
-								online.push(this_player);
-							}
 							else {
-								unavailable.push(this_player);
+								switch(this_player.presence.status) {
+								case 'online':
+									online.push(this_player);
+									break;
+								case 'offline':
+									offline.push(this_player);
+									break;
+								default:
+									unavailable.push(this_player);
+									break;
+								}
 							}
 						}
 					}
 				}
-				embed.addField(`In Game: ${in_game.length}`, in_game.length > 0 ? in_game.join(', ') : 'No players found.');
-				embed.addField(`Online: ${online.length}`, online.length > 0 ? online.join(', ') : 'No players found.');
-				embed.addField(`Away / Busy / Offline : ${unavailable.length}`, unavailable.length > 0 ? unavailable.join(', ') : 'No players found.');
-				embed.setFooter(`${players.length} players total.`);
+				if (in_game.length > 0)	embed.addField(`In Game: ${in_game.length}`, in_game.join(', '));
+				if (online.length > 0)	embed.addField(`Online: ${online.length}`, online.join(', '));
+				if (unavailable.length > 0)	embed.addField(`Away or Busy: ${unavailable.length}`, unavailable.join(', '));
+				if (offline.length > 0)	embed.addField(`Offline: ${offline.length}`, offline.join(', '));
+				embed.setFooter(`A total of ${players.length} player${players.length > 1 ? 's were' : ' was'} found.`);
 				embed.setColor('#25ff00');
 				message_manager.sendToChannel(message.channel, embed).then(this_message => {
 					this_message.delete({ timeout: 30000 }).catch(e => void e);
