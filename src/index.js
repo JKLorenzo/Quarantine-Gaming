599624ef54edb4ee8762554cb506befb05ec0026
Juruel Keanu Lorenzo <39931559+JKLorenzo@users.noreply.github.com>
2021-02-26 17:19:04 +0800

    Initial Canvas Integration / CompatCheck on Heroku

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 const { CommandoClient } = require('discord.js-commando');
 const Discord = require('discord.js');
+const Canvas = require('canvas');
 const path = require('path');
 const app = require('./modules/app.js');
 const fs = require('fs');
@@ -86,6 +87,63 @@ async function uploadLocalEmojis() {
 	}
 }
 
+/**
+ *  @param {Canvas.Canvas} canvas
+ *	@param {String} text
+*/
+const applyText = (canvas, text) => {
+	const ctx = canvas.getContext('2d');
+
+	// Declare a base size of the font
+	let fontSize = 70;
+
+	do {
+		// Assign the font to the context and decrement it so it can be measured again
+		ctx.font = `${fontSize -= 10}px sans-serif`;
+		// Compare pixel width of the text to the canvas minus the approximate avatar size
+	} while (ctx.measureText(text).width > canvas.width - 300);
+
+	// Return the result to use in the actual canvas
+	return ctx.font;
+};
+
+async function CanvasHerokuCompatibilityTest() {
+	const guild = client.guilds.cache.first();
+	const member = guild.me;
+	const testing_grounds = guild.channels.cache.get(constants.channels.qg.testing_ground_text);
+
+	const canvas = Canvas.createCanvas(700, 250);
+	const ctx = canvas.getContext('2d');
+
+	const background = await Canvas.loadImage('./background.png');
+	ctx.drawImage(background, 0, 0, canvas.width, canvas.height);
+
+	ctx.strokeStyle = '#74037b';
+	ctx.strokeRect(0, 0, canvas.width, canvas.height);
+
+	// Slightly smaller text placed above the member's display name
+	ctx.font = '28px sans-serif';
+	ctx.fillStyle = '#ffffff';
+	ctx.fillText('Welcome to the server,', canvas.width / 2.5, canvas.height / 3.5);
+
+	// Add an exclamation point here and below
+	ctx.font = applyText(canvas, `${member.displayName}!`);
+	ctx.fillStyle = '#ffffff';
+	ctx.fillText(`${member.displayName}!`, canvas.width / 2.5, canvas.height / 1.8);
+
+	ctx.beginPath();
+	ctx.arc(125, 125, 100, 0, Math.PI * 2, true);
+	ctx.closePath();
+	ctx.clip();
+
+	const avatar = await Canvas.loadImage(member.user.displayAvatarURL({ format: 'jpg' }));
+	ctx.drawImage(avatar, 25, 25, 200, 200);
+
+	const attachment = new Discord.MessageAttachment(canvas.toBuffer(), 'welcome-image.png');
+
+	await testing_grounds.send(`Welcome to the server, ${member}!`, attachment);
+}
+
 client.once('ready', async () => {
 	console.log('Startup: Initializing');
 	try {
@@ -102,8 +160,9 @@ client.once('ready', async () => {
 		role_manager.initialize(client);
 		speech.initialize(client);
 		error_manager.initialize(client);
-		await app.initialize(client);
+		// await app.initialize(client);
 		uploadLocalEmojis();
+		await CanvasHerokuCompatibilityTest();
 	}
 	catch (error) {
 		console.error(`Startup: Failed with error ${error}`);
