1601813bcce1e58f2a685c93f308b09574662ec6
Juruel Keanu Lorenzo <39931559+JKLorenzo@users.noreply.github.com>
2021-03-19 13:16:18 +0800

    feat(ErrorManager): Optimize error reporting

--- src/modules/error_manager.js
+++ src/modules/error_manager.js
@@ -40,55 +40,35 @@ module.exports.mark = async (ErrorTicket) => {
 
 			const epm = errors_per_minute.length;
 
-			if (!threshold_reached) {
-				const embed = new Discord.MessageEmbed();
-				embed.setAuthor('Quarantine Gaming: Telemetry');
-				embed.setTitle('Exception Details');
-				if (ErrorTicket.name) {
-					embed.addField('Function', ErrorTicket.name);
-				}
-				if (ErrorTicket.error) {
-					embed.addField('Message', ErrorTicket.error);
-				}
-				if (ErrorTicket.location) {
-					embed.addField('Location', ErrorTicket.location);
-				}
-				if (ErrorTicket.error.code) {
-					embed.addField('Code', ErrorTicket.error.code);
-				}
-				embed.addField('Errors per Minute', epm);
-				embed.addField('Threshold Hit', threshold_reached ? 'True' : 'False');
-				embed.addField('Threshold Hit Count', threshold_hit_count);
-				embed.setThumbnail('https://mir-s3-cdn-cf.behance.net/project_modules/disp/c9955d46715833.589222657aded.png');
-				embed.setColor('#FF0000');
-				await message_manager.sendToChannel(constants.channels.qg.logs, embed);
-			}
-
 			if ((epm > 5 || (ErrorTicket.error.code != null && ErrorTicket.error.code == '500')) && !threshold_reached) {
 				// Change bot presence
-				await app.setActivity(`SERVER RESTART (${++threshold_hit_count})`, 'WATCHING');
-
-				// Announce
-				await message_manager.sendToChannel(constants.channels.server.announcements, '**Discord Status Updates**\nDiscord is currently having some issues and may impact users on this server. Visit <https://discordstatus.com/> for more info.').catch(async () => {
-					const embed = new Discord.MessageEmbed();
-					embed.setAuthor('Limited Functionality');
-					embed.setTitle('Discord Status Updates');
-					embed.setDescription('Discord is currently having some issues and may impact users on this server. Visit <https://discordstatus.com/> for more info.');
-					embed.setColor('ffe300');
-					await message_manager.sendToChannel(constants.channels.server.announcements, embed);
-				});
+				app.setActivity(`SERVER RESTART (${++threshold_hit_count})`, 'WATCHING');
 
 				// Notify staffs
-				await message_manager.sendToChannel(constants.channels.server.management, 'I\'m currently detecting issues with Discord; some functionalities are disabled. A bot restart is recommended once the issues are resolved.').catch(async () => {
+				message_manager.sendToChannel(constants.channels.server.management, 'I\'m currently detecting issues with Discord; some functionalities are disabled. A bot restart is recommended once the issues are resolved.').catch(async () => {
 					const embed = new Discord.MessageEmbed();
 					embed.setAuthor('Limited Functionality');
 					embed.setTitle('Issues with Discord');
 					embed.setDescription('I\'m currently detecting issues with Discord; some functionalities are disabled. A bot restart is recommended once the issues are resolved.');
 					embed.setColor('ffe300');
-					await message_manager.sendToChannel(constants.channels.server.management, embed);
+					message_manager.sendToChannel(constants.channels.server.management, embed);
 				});
 				threshold_reached = true;
 			}
+
+			const embed = new Discord.MessageEmbed();
+			embed.setAuthor('Quarantine Gaming: Telemetry');
+			embed.setTitle('Exception Details');
+			if (ErrorTicket.name) embed.addField('Method', ErrorTicket.name, true);
+			if (ErrorTicket.location) embed.addField('Location', ErrorTicket.location, true);
+			if (ErrorTicket.error.code) embed.addField('Error Code', ErrorTicket.error.code, true);
+			if (ErrorTicket.error) embed.addField('Error Message', ErrorTicket.error);
+			embed.addField('EPM', epm, true);
+			embed.addField('TH', threshold_reached ? 'True' : 'False', true);
+			embed.addField('THC', threshold_hit_count, true);
+			embed.setThumbnail('https://mir-s3-cdn-cf.behance.net/project_modules/disp/c9955d46715833.589222657aded.png');
+			embed.setColor('#FF0000');
+			message_manager.sendToChannel(constants.channels.qg.logs, embed);
 		}
 		catch (error) {
 			console.error(`ErrorManager: Failed with error ${error}`);
