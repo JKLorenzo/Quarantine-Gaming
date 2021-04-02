3bcfe6c052703b78950d51d7f9636d638823f3d1
Juruel Keanu Lorenzo <39931559+JKLorenzo@users.noreply.github.com>
2021-04-02 17:44:38 +0800

    feat: Utils

---
+++ src/utils/ProcessQueue.js
@@ -0,0 +1,80 @@
+const { sleep } = require('./Base.js');
+
+module.exports = class ProcessQueue {
+	/**
+     * Creates a process queuer.
+     * @param {Number} timeout Timeout between function execution in ms.
+     */
+	constructor(timeout = 0) {
+		/**
+		 * @readonly
+		 * The timeout between function execution in ms.
+		 */
+		this.timeout = timeout;
+		/**
+          * @readonly
+          * The total number of IDs this manager used.
+          */
+		this.totalID = 0;
+		/**
+          * @readonly
+          * The ID that is currently being executed.
+          */
+		this.currentID = 0;
+		/**
+          * @readonly
+          * The state of this manager.
+          */
+		this.running = false;
+		/**
+          * @private
+          * The array containing the functions to be executed by this manager.
+          * @type {Array<{function: Function, promise: {resolve: Function, reject: Function}}>}
+          */
+		this.array = new Array();
+	}
+
+	/**
+	 * @public
+	 * Adds the function to the queue.
+	 * @param {Function} the_function The function to be queued.
+	 * @returns {Promise} The return object of the queued function.
+	 */
+	queue(the_function) {
+		this.totalID++;
+		let res, rej;
+		const promise = new Promise((resolve, reject) => {
+			res = resolve;
+			rej = reject;
+		});
+		this.array.push({
+			function: the_function,
+			promise: {
+				resolve: res,
+				reject: rej,
+			},
+		});
+		if (!this.running) this.run();
+		return promise;
+	}
+
+	/**
+	 * @private
+	 * Executes all the items in the array.
+	 */
+	async run() {
+		this.running = true;
+		while (this.array.length > 0) {
+			const data = this.array.shift();
+			try {
+				data.promise.resolve(await data.function());
+			}
+			catch(error) {
+				data.promise.reject(error);
+			}
+			this.currentID++;
+			if (this.timeout > 0) await sleep(this.timeout);
+		}
+		this.running = false;
+	}
+};
