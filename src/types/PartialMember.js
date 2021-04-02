f4ec564fad8d15852622ac1198fd309810ef1024
Juruel Keanu Lorenzo <39931559+JKLorenzo@users.noreply.github.com>
2021-04-02 17:44:19 +0800

    feat: Types

---
+++ src/types/PartialMember.js
@@ -0,0 +1,38 @@
+/**
+ * A Member Object represented on the database.
+ */
+class PartialMember {
+	/**
+     * Initializes this member.
+     * @param {{id: String, name: String, tagname: String, inviter?: String, moderator?: String}} data
+     */
+	constructor(data = {}) {
+		/**
+		 * @readonly
+		 * @type {String}
+		 */
+		this.id = data.id;
+		/**
+		 * @readonly
+		 * @type {String}
+		 */
+		this.name = data.name;
+		/**
+		 * @readonly
+		 * @type {String}
+		 */
+		this.tagname = data.tagname;
+		/**
+		 * @readonly
+		 * @type {String}
+		 */
+		this.inviter = data.inviter;
+		/**
+		 * @readonly
+		 * @type {String}
+		 */
+		this.moderator = data.moderator;
+	}
+}
+
+module.exports = PartialMember;
