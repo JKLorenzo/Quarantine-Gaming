f2ab7f433fb91a3bf65ce3723c8a89106f1e50ce
Juruel Keanu Lorenzo <39931559+JKLorenzo@users.noreply.github.com>
2021-04-18 13:45:06 +0800

    feat(*): Rename actions to methods

--- src/methods/ScreenMember.js
+++ src/methods/ScreenMember.js
@@ -1,14 +1,21 @@
 const Discord = require('discord.js');
+const { compareDate, constants } = require('../utils/Base.js');
 
 /**
- * @param {import('../app.js')} app
+ * @typedef {import('../structures/Base.js').Client} Client
+ * @typedef {import('../structures/Base.js').ExtendedMember} ExtendedMember
+ */
+
+/**
+ * @param {Client} client
  * @param {Discord.GuildMember} member
  */
-module.exports = async function ScreenMember(app, member) {
+module.exports = async function ScreenMember(client, member) {
 	const created_day = member.user.createdAt;
-	const created_day_difference = app.utils.compareDate(created_day);
+	const created_day_difference = compareDate(created_day);
 
-	const inviters = await app.getInvites();
+	// !!!!!!!!!!! INVITER MANAGER MISSING !!!!!!!!!!!
+	const inviters = [];
 	const embed = new Discord.MessageEmbed();
 	embed.setAuthor('Quarantine Gaming: Member Approval');
 	embed.setTitle('User Details');
@@ -27,18 +34,18 @@ module.exports = async function ScreenMember(app, member) {
 		const MessageToSend = new Array();
 		MessageToSend.push(`Hi ${member.user.username}, and welcome to **Quarantine Gaming**!`);
 		MessageToSend.push('Please wait while we are processing your membership approval.');
-		await app.message_manager.sendToUser(member, MessageToSend.join('\n'));
+		await client.message_manager.sendToUser(member, MessageToSend.join('\n'));
 
 		embed.addField('Actions:', '✅ - Approve     ❌ - Kick     ⛔ - Ban');
 		reactions = ['✅', '❌', '⛔'];
 	}
 	embed.setColor('#25c059');
-	const message = await app.message_manager.sendToChannel(app.utils.constants.channels.server.management, {
-		content: `${member} wants to join this server. ${app.role(app.utils.constants.roles.staff)} or ${app.role(app.utils.constants.roles.moderator)} action is required.`,
+	const message = await client.message_manager.sendToChannel(constants.channels.server.management, {
+		content: `${member} wants to join this server. ${client.role(constants.roles.staff)} or ${client.role(constants.roles.moderator)} action is required.`,
 		embed: embed,
 	});
 	for (const emoji of reactions) {
-		app.reaction_manager.add(message, emoji);
+		client.reaction_manager.add(message, emoji);
 	}
 	return message;
 };
