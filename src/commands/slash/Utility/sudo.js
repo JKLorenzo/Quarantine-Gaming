73ff95f182e7f714bd6b6bbce7731783c427beb8
Juruel Keanu Lorenzo <39931559+JKLorenzo@users.noreply.github.com>
2021-05-23 19:42:02 +0800

    feat(Commands): Add sudo slash command

---
+++ src/commands/slash/Utility/sudo.js
@@ -0,0 +1,124 @@
+import util from 'util';
+import tags from 'common-tags';
+import discord from 'discord.js';
+import { SlashCommand } from '../../../structures/Base.js';
+import { constants } from '../../../utils/Base.js';
+
+/**
+ * @typedef {import('discord.js').CommandInteraction} CommandInteraction
+ * @typedef {import('../../../structures/Base.js').Client} Client
+ */
+
+const nl = '!!NL!!';
+const nlPattern = new RegExp(nl, 'g');
+
+export default class Sudo extends SlashCommand {
+	constructor() {
+		super({
+			name: 'sudo',
+			description: '[Owner] Execute a command as a superuser.',
+			options: [
+				{
+					name: 'command',
+					description: 'Enter the command to execute.',
+					type: 'STRING',
+					required: true,
+				},
+			],
+			defaultPermission: false,
+			permissions: {
+				users: {
+					allow: [
+						constants.owner,
+					],
+				},
+			},
+		});
+
+		this.lastResult = null;
+		Object.defineProperty(this, '_sensitivePattern', { value: null, configurable: true });
+	}
+
+	/**
+     * @param {CommandInteraction} interaction
+     * @param {{command: String}} options
+     */
+	async exec(interaction, options) {
+		await interaction.defer(true);
+
+		/** @type {Client} */
+		const client = interaction.client;
+		this.client = client;
+		let command = options.command;
+
+		// eslint-disable-next-line no-unused-vars
+		const lastResult = this.lastResult;
+
+		// Remove any surrounding code blocks before evaluation
+		if(command.startsWith('```') && command.endsWith('```')) {
+			command = command.replace(/(^.*?\s)|(\n.*$)/g, '');
+		}
+
+		// Run the code and measure its execution time
+		let hrDiff;
+		try {
+			const hrStart = process.hrtime();
+			this.lastResult = eval(command);
+			hrDiff = process.hrtime(hrStart);
+		} catch(err) {
+			return interaction.editReply(`Error while evaluating: \`${err}\``);
+		}
+
+		// Prepare for callback time and respond
+		this.hrStart = process.hrtime();
+		const result = this.makeResultMessages(this.lastResult, hrDiff, command);
+		if(Array.isArray(result)) {
+			return result.map(item => interaction.editReply(item));
+		} else {
+			return interaction.editReply(result);
+		}
+	}
+
+	escapeRegex(str) {
+		return str.replace(/[|\\{}()[\]^$+*?.]/g, '\\$&');
+	}
+
+	makeResultMessages(result, hrDiff, input = null) {
+		const inspected = util.inspect(result, { depth: 0 })
+			.replace(nlPattern, '\n')
+			.replace(this.sensitivePattern, '--snip--');
+		const split = inspected.split('\n');
+		const last = inspected.length - 1;
+		const prependPart = inspected[0] !== '{' && inspected[0] !== '[' && inspected[0] !== '\'' ? split[0] : inspected[0];
+		const appendPart = inspected[last] !== '}' && inspected[last] !== ']' && inspected[last] !== '\'' ?
+			split[split.length - 1] :
+			inspected[last];
+		const prepend = `\`\`\`javascript\n${prependPart}\n`;
+		const append = `\n${appendPart}\n\`\`\``;
+		if(input) {
+			return discord.splitMessage(tags.stripIndents`
+				*Executed in ${hrDiff[0] > 0 ? `${hrDiff[0]}s ` : ''}${hrDiff[1] / 1000000}ms.*
+				\`\`\`javascript
+				${inspected}
+				\`\`\`
+			`, { maxLength: 1900, prepend, append });
+		} else {
+			return discord.splitMessage(tags.stripIndents`
+				*Callback executed after ${hrDiff[0] > 0 ? `${hrDiff[0]}s ` : ''}${hrDiff[1] / 1000000}ms.*
+				\`\`\`javascript
+				${inspected}
+				\`\`\`
+			`, { maxLength: 1900, prepend, append });
+		}
+	}
+
+	get sensitivePattern() {
+		if(!this._sensitivePattern) {
+			const client = this.client;
+			let pattern = '';
+			if(client.token) pattern += this.escapeRegex(client.token);
+			Object.defineProperty(this, '_sensitivePattern', { value: new RegExp(pattern, 'gi'), configurable: false });
+		}
+		return this._sensitivePattern;
+	}
+}
