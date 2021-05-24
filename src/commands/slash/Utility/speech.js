494c43e13bf6a07ae48d3f4a221742a77d3df32f
Juruel Keanu Lorenzo <39931559+JKLorenzo@users.noreply.github.com>
2021-05-24 16:10:33 +0800

    feat(Commands): Add speech slash command

---
+++ src/commands/slash/Utility/speech.js
@@ -0,0 +1,41 @@
+import { SlashCommand } from '../../../structures/Base.js';
+
+/**
+ * @typedef {import('discord.js').VoiceChannel} VoiceChannel
+ * @typedef {import('discord.js').CommandInteraction} CommandInteraction
+ */
+
+export default class Speech extends SlashCommand {
+	constructor() {
+		super({
+			name: 'speech',
+			description: 'Say a message to a voice channel using Quarantine Gaming\'s TTS.',
+			options: [
+				{
+					name: 'voice_channel',
+					description: 'Select the target voice channel',
+					type: 'CHANNEL',
+					required: true,
+				},
+				{
+					name: 'message',
+					description: 'Enter the message to be coverted as speech.',
+					type: 'STRING',
+					required: true,
+				},
+			],
+		});
+	}
+
+	/**
+     *
+     * @param {CommandInteraction} interaction
+     * @param {{voice_channel: VoiceChannel, message: String}} options
+     */
+	async exec(interaction, options) {
+		if (options.voice_channel.isText()) return interaction.reply('You must select a valid voice channel.', { ephemeral: true });
+		await interaction.defer(true);
+		await this.client.speech_manager.say(options.voice_channel, options.message);
+		interaction.editReply(`Speech was delivered successfully to ${options.voice_channel}.`);
+	}
+}
