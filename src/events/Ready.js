0bb2305635769b6983562c82f5631cf88a0df2ee
Juruel Keanu Lorenzo <39931559+JKLorenzo@users.noreply.github.com>
2021-05-29 01:05:02 +0800

    feat(Ready): update startup message

--- src/events/Ready.js
+++ src/events/Ready.js
@@ -7,7 +7,7 @@ import { constants } from '../utils/Base.js';
 /** @param {Client} client */
 export default async function onceReady(client) {
 	console.log('Client logged in. Initializing...');
-	client.message_manager.sendToChannel(constants.interface.channels.logs, '<--------------**ONLINE**-------------->');
+	client.message_manager.sendToChannel(constants.interface.channels.logs, '[-- **ONLINE**  -------------------------->');
 
 	await client.database_manager.init();
 	await client.gateway_manager.init();
@@ -19,7 +19,7 @@ export default async function onceReady(client) {
 
 	client.free_game_manager.actions.start();
 
-	client.message_manager.sendToChannel(constants.interface.channels.logs, '<-----------**INITIALIZED**----------->');
+	client.message_manager.sendToChannel(constants.interface.channels.logs, '[-- **INITIALIZED**  -------------------->');
 
 	console.log('Client initialized.');
 }
