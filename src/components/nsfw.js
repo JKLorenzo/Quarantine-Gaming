0f41e9ca6014107f722239289ae2f31c5eac97f5
Juruel Keanu Lorenzo <39931559+JKLorenzo@users.noreply.github.com>
2021-05-29 18:39:00 +0800

    feat(components): add nsfw component

---
+++ src/components/nsfw.js
@@ -0,0 +1,47 @@
+import { MessageActionRow, MessageButton } from 'discord.js';
+import { MessageComponent } from '../structures/Base.js';
+import { constants } from '../utils/Base.js';
+
+/**
+ * @typedef {import('discord.js').MessageComponentInteraction} MessageComponentInteraction
+ */
+
+export default class NotSafeForWork extends MessageComponent {
+	constructor() {
+		super({
+			name: 'nsfw',
+			options: [
+				new MessageActionRow({
+					components: [
+						new MessageButton({
+							customID: 'button',
+							label: 'Enable or Disable NSFW Content',
+							style: 'SECONDARY',
+						}),
+					],
+				}),
+			],
+		});
+	}
+
+	async init(client) {
+		this.client = client;
+
+		this.options[0].components[0].setEmoji(this.client.guild.emojis.cache.find(e => e.name === 'pepe_peek'));
+
+		return this;
+	}
+
+	/**
+     * @param {MessageComponentInteraction} interaction
+     */
+	async exec(interaction) {
+		const member = this.client.member(interaction.member);
+
+		member.hasRole(constants.roles.nsfw)
+			? await this.client.role_manager.remove(member, constants.roles.nsfw)
+			: await this.client.role_manager.add(member, constants.roles.nsfw);
+
+		await interaction.deferUpdate();
+	}
+}
