375f84080f352b14d38812fc478742e8c8678c4a
Juruel Keanu Lorenzo <39931559+JKLorenzo@users.noreply.github.com>
2021-06-03 15:12:13 +0800

    refactor(main): conform to eslint rules

--- src/main.js
+++ src/main.js
@@ -3,52 +3,56 @@ import express from 'express';
 import { Client, ExtendedMessage } from './structures/Base.js';
 
 const {
-	DIRECT_MESSAGES, GUILDS, GUILD_BANS, GUILD_EMOJIS, GUILD_INVITES, GUILD_MEMBERS,
-	GUILD_MESSAGES, GUILD_MESSAGE_REACTIONS, GUILD_PRESENCES, GUILD_VOICE_STATES,
+  DIRECT_MESSAGES,
+  GUILDS,
+  GUILD_BANS,
+  GUILD_EMOJIS,
+  GUILD_INVITES,
+  GUILD_MEMBERS,
+  GUILD_MESSAGES,
+  GUILD_MESSAGE_REACTIONS,
+  GUILD_PRESENCES,
+  GUILD_VOICE_STATES,
 } = Intents.FLAGS;
 
 Structures.extend('Message', () => ExtendedMessage);
 
 const app = express();
 const client = new Client({
-	allowedMentions: {
-		parse: [
-			'everyone', 'roles', 'users',
-		],
-		repliedUser: true,
-	},
-	intents: [
-		DIRECT_MESSAGES,
-		GUILDS,
-		GUILD_BANS,
-		GUILD_EMOJIS,
-		GUILD_INVITES,
-		GUILD_MEMBERS,
-		GUILD_MESSAGES,
-		GUILD_MESSAGE_REACTIONS,
-		GUILD_PRESENCES,
-		GUILD_VOICE_STATES,
-	],
-	partials: [
-		'MESSAGE', 'CHANNEL', 'REACTION',
-	],
-	presence: {
-		activities: [
-			{
-				name: '/commands',
-				type: 'LISTENING',
-			},
-		],
-		status: 'online',
-		afk: false,
-	},
+  allowedMentions: {
+    parse: ['everyone', 'roles', 'users'],
+    repliedUser: true,
+  },
+  intents: [
+    DIRECT_MESSAGES,
+    GUILDS,
+    GUILD_BANS,
+    GUILD_EMOJIS,
+    GUILD_INVITES,
+    GUILD_MEMBERS,
+    GUILD_MESSAGES,
+    GUILD_MESSAGE_REACTIONS,
+    GUILD_PRESENCES,
+    GUILD_VOICE_STATES,
+  ],
+  partials: ['MESSAGE', 'CHANNEL', 'REACTION'],
+  presence: {
+    activities: [
+      {
+        name: '/commands',
+        type: 'LISTENING',
+      },
+    ],
+    status: 'online',
+    afk: false,
+  },
 });
 
 app.get('/', (req, res) => {
-	res.send(client?.ws?.ping ? 'online' : 'offline');
+  res.send(client?.ws?.ping ? 'online' : 'offline');
 });
 
 app.listen(process.env.PORT || 3000, () => {
-	console.log('Server is running...');
-	client.login(process.env.BOT_TOKEN);
+  console.log('Server is running...');
+  client.login(process.env.BOT_TOKEN);
 });
