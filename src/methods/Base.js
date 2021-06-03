9ce214a16be9b0ad50c6b4aa818b2d385995d0ee
Juruel Keanu Lorenzo <39931559+JKLorenzo@users.noreply.github.com>
2021-06-03 15:14:28 +0800

    refactor(methods): conform to eslint rules

--- src/methods/Base.js
+++ src/methods/Base.js
@@ -8,25 +8,33 @@ import voiceChannelTransfer from './VoiceChannelTransfer.js';
  */
 
 export default class BaseMethods {
-	/** @param {Client} client */
-	constructor(client) {
-		this.client = client;
-	}
+  /**
+   * @param {Client} client The QG Client
+   */
+  constructor(client) {
+    this.client = client;
+  }
 
-	/**
-	 * Fetches an image online or from the database when it exists.
-	 * @param {String} title
-	 */
-	fetchImage(title) {
-		return fetchImage(this.client, title);
-	}
+  /**
+   * Fetches an image online or from the database when it exists.
+   * @param {string} title The title of the image to be searched
+   * @typedef {Object} ImageData
+   * @property {string} [small]
+   * @property {string} [large]
+   * @returns {Promise<ImageData>}
+   */
+  fetchImage(title) {
+    return fetchImage(this.client, title);
+  }
 
-	/**
-	 * Transfers a member to another voice channel.
- 	 * @param {VoiceChannel} channel
- 	 * @param {GuildMember[]} members
- 	 */
-	voiceChannelTransfer(channel, member) {
-		return voiceChannelTransfer(channel, member);
-	}
+  /**
+   * Transfers a member to another voice channel.
+   * @param {VoiceChannel} channel The channel where the members will be transferred
+   * @param {GuildMember[]} members The members to transfer
+   * @param {string} [message] The message to send after a successfull transfer
+   * @returns {Promise<null>}
+   */
+  voiceChannelTransfer(channel, members, message) {
+    return voiceChannelTransfer(this.client, channel, members, message);
+  }
 }
--- src/methods/FetchImage.js
+++ src/methods/FetchImage.js
@@ -6,43 +6,46 @@ import { searchImage } from '../utils/Base.js';
 
 /**
  * Fetches an image online or from the database when it exists.
- * @param {Client} client
- * @param {String} title
- * @returns {{small?: String, large?: String}}
+ * @param {Client} client The QG Client
+ * @param {string} title The title of the image to be searched
+ * @typedef {Object} ImageData
+ * @property {string} [small]
+ * @property {string} [large]
+ * @returns {Promise<ImageData>}
  */
 export default async function fetchImage(client, title) {
-	let images = client.database_manager.getImage(title);
-	if (!images) {
-		images = {};
-		const result = await Promise.all([
-			searchImage(`${title} game logo`, {
-				ratio: 1,
-				minWidth: 100,
-				minHeight: 100,
-			}),
-			searchImage(`${title} game background`, {
-				ratio: 1.7,
-				minWidth: 1000,
-				minHeight: 1000,
-			}),
-		]);
-		images.small = result[0];
-		images.large = result[1];
-		client.database_manager.updateImage(title, images);
-	} else if (!images.small) {
-		images.small = await searchImage(`${title} game logo`, {
-			ratio: 1,
-			minWidth: 100,
-			minHeight: 100,
-		});
-		client.database_manager.updateImage(title, { small: images.small });
-	} else if (!images.large) {
-		images.large = await searchImage(`${title} game background`, {
-			ratio: 1.7,
-			minWidth: 1000,
-			minHeight: 1000,
-		});
-		client.database_manager.updateImage(title, { large: images.large });
-	}
-	return images;
+  let images = client.database_manager.getImage(title);
+  if (!images) {
+    images = {};
+    const result = await Promise.all([
+      searchImage(`${title} game logo`, {
+        ratio: 1,
+        minWidth: 100,
+        minHeight: 100,
+      }),
+      searchImage(`${title} game background`, {
+        ratio: 1.7,
+        minWidth: 1000,
+        minHeight: 1000,
+      }),
+    ]);
+    images.small = result[0];
+    images.large = result[1];
+    client.database_manager.updateImage(title, images);
+  } else if (!images.small) {
+    images.small = await searchImage(`${title} game logo`, {
+      ratio: 1,
+      minWidth: 100,
+      minHeight: 100,
+    });
+    client.database_manager.updateImage(title, { small: images.small });
+  } else if (!images.large) {
+    images.large = await searchImage(`${title} game background`, {
+      ratio: 1.7,
+      minWidth: 1000,
+      minHeight: 1000,
+    });
+    client.database_manager.updateImage(title, { large: images.large });
+  }
+  return images;
 }
