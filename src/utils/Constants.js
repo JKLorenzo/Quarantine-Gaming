0608a2494f91eeccee7666f16f6fa12861afd2f1
Juruel Keanu Lorenzo <39931559+JKLorenzo@users.noreply.github.com>
2021-06-03 15:15:28 +0800

    refactor(utils): conform to eslint rules

--- src/utils/Constants.js
+++ src/utils/Constants.js
@@ -1,98 +1,112 @@
+/* eslint-disable max-len */
+
 export default {
-	me: '782308458803232842',
-	owner: '393013053488103435',
-	qg: {
-		guild: '351178660725915649',
-		roles: {
-			// main
-			staff: '793318727223476245',
-			moderator: '839185730399764511',
-			booster: '757461486934163517',
-			member: '722699433225224233',
-			everyone: '351178660725915649',
-			// integrations
-			nsfw: '700481554132107414',
-			streaming: '757128062276993115',
-			// bots
-			bot: '699681300075905046',
-			music_bot: '700397445506531358',
-			nsfw_bot: '700486309655085107',
-			// free game updates
-			steam: '722645979248984084',
-			epic: '722691589813829672',
-			gog: '722691679542312970',
-			console: '722691724572491776',
-			ubisoft: '750517524738605087',
-			xbox: '848035837533880322',
-			playstation: '848036062336778273',
-			wii: '848155133699293234',
-		},
-		channels: {
-			category: {
-				server: '699787533172604958',
-				staff: '762553424444784691',
-				text: '828620371195985962',
-				voice: '782163385910951936',
-				integrations: '778541774154891264',
-				dedicated: '822083041086275645',
-				dedicated_voice: '749231470396309535',
-				qg: '771646102252027904',
-				archives: '749661721014042774',
-			},
-			server: {
-				announcements: '759920653146652682',
-				rules: '757299898550059129',
-				roles: '700134489170378872',
-				guides: '716710573471039519',
-				suggestions: '700126421682618389',
-				management: '749763548090990613',
-				logs: '722760285622108210',
-			},
-			staff: {
-				general: '774785123866247228',
-				pandoras_box: '747005488197009568',
-			},
-			text: {
-				general: '749661539908190258',
-				memes: '699792119077928960',
-				explicit: '699847972623482931',
-				music_commands: '699782177378009108',
-			},
-			integrations: {
-				free_games: '699763763859161108',
-				game_invites: '759755324264808489',
-				game_updates: '781509022972248064',
-				afk: '663443529170681857',
-			},
-		},
-	},
-	cs: {
-		guild: '806481618008539137',
-		channels: {
-			dm: '843521609221799976',
-			game: '843857933049397298',
-			gateway: '843495328409845790',
-			game_events: '844576930631122974',
-			gateway_events: '843495540883980308',
-			member_events: '844049068488785980',
-			logs: '842696900235100190',
-			telemetry: '842698906067927060',
-		},
-	},
-	images: {
-		free_games_banner: 'https://media.playstation.com/is/image/SCEA/playstation-vue-hero-banner-desktop-01-us-22jan19?$native_nt$',
-		free_games_banner_1: 'https://th.bing.com/th/id/Refb29ff6809187d2cba9e340367d66f7?rik=16Td9AQg3LL7Cg&riu=http%3a%2f%2f2.bp.blogspot.com%2f-brXtJ3udkBk%2fUCnE_REJjsI%2fAAAAAAAACDU%2faDXC9brYZMo%2fs1600%2f46647-game-banner-full.png&ehk=4cYtiI0VSSFC8NdDjgGFDu97FcW%2fsYjKzsg4ILAB2W0%3d&risl=&pid=ImgRaw',
-		nsfw_banner: 'https://yesofcorsa.com/wp-content/uploads/2016/09/shokugeki_no_soma_wallpaper_download_3.png',
-		multiplayer_banner: 'https://playbingerepeat.files.wordpress.com/2018/05/fortnite-banner.png?w=1332&h=666&crop=1',
-		audio_control_thumbnail: 'http://www.extensions.in.th/amitiae/2013/prefs/images/sound_icon.png',
-		error_message_thumbnail: 'https://mir-s3-cdn-cf.behance.net/project_modules/disp/c9955d46715833.589222657aded.png',
-		ratelimit_thumbnail: 'https://th.bing.com/th/id/Rf7832050f813fd4298d1e088f0f3fa55?rik=7Xq2EbPkqJZkOA&riu=http%3a%2f%2fwikitravel.org%2fupload%2fen%2fb%2fb2%2fWarning-icon.png&ehk=PkXja2hMq1jutYB%2b0GFtPqmAz5kDFDNF46LFCMRThhs%3d&risl=&pid=ImgRaw',
-		error_thumbnail: 'https://upload.wikimedia.org/wikipedia/commons/thumb/2/27/Alert-Stop-Warning-Error_icon.svg/1024px-Alert-Stop-Warning-Error_icon.svg.png',
-		warning_thumbnail: 'https://th.bing.com/th/id/R56a2c39ff1eb81159883eb0199a23a55?rik=P0sSInV4jnLJ4A&riu=http%3a%2f%2ficons.iconarchive.com%2ficons%2fcreative-freedom%2fshimmer%2f256%2fWarning-icon.png&ehk=%2bzahp%2f2lHxKQmBVmJvWgliQGudrOs9%2ftdVwSnN5FQPM%3d&risl=&pid=ImgRaw',
-	},
-	colors: {
-		play_role: '#7b00ff',
-		game_role: '#00ffff',
-		game_role_mentionable: '#00fffe',
-	},
+  me: '782308458803232842',
+  owner: '393013053488103435',
+  qg: {
+    guild: '351178660725915649',
+    roles: {
+      // Main
+      staff: '793318727223476245',
+      moderator: '839185730399764511',
+      booster: '757461486934163517',
+      member: '722699433225224233',
+      everyone: '351178660725915649',
+      // Integrations
+      nsfw: '700481554132107414',
+      streaming: '757128062276993115',
+      // Bots
+      bot: '699681300075905046',
+      music_bot: '700397445506531358',
+      nsfw_bot: '700486309655085107',
+      // Free game updates
+      steam: '722645979248984084',
+      epic: '722691589813829672',
+      gog: '722691679542312970',
+      console: '722691724572491776',
+      ubisoft: '750517524738605087',
+      xbox: '848035837533880322',
+      playstation: '848036062336778273',
+      wii: '848155133699293234',
+    },
+    channels: {
+      category: {
+        server: '699787533172604958',
+        staff: '762553424444784691',
+        text: '828620371195985962',
+        voice: '782163385910951936',
+        integrations: '778541774154891264',
+        dedicated: '822083041086275645',
+        dedicated_voice: '749231470396309535',
+        qg: '771646102252027904',
+        archives: '749661721014042774',
+      },
+      server: {
+        announcements: '759920653146652682',
+        rules: '757299898550059129',
+        roles: '700134489170378872',
+        guides: '716710573471039519',
+        suggestions: '700126421682618389',
+        management: '749763548090990613',
+        logs: '722760285622108210',
+      },
+      staff: {
+        general: '774785123866247228',
+        pandoras_box: '747005488197009568',
+      },
+      text: {
+        general: '749661539908190258',
+        memes: '699792119077928960',
+        explicit: '699847972623482931',
+        music_commands: '699782177378009108',
+      },
+      integrations: {
+        free_games: '699763763859161108',
+        game_invites: '759755324264808489',
+        game_updates: '781509022972248064',
+        afk: '663443529170681857',
+      },
+    },
+  },
+  cs: {
+    guild: '806481618008539137',
+    channels: {
+      dm: '843521609221799976',
+      game: '843857933049397298',
+      gateway: '843495328409845790',
+      game_events: '844576930631122974',
+      gateway_events: '843495540883980308',
+      member_events: '844049068488785980',
+      logs: '842696900235100190',
+      telemetry: '842698906067927060',
+    },
+  },
+  images: {
+    /* eslint-disable prettier/prettier */
+    free_games_banner:
+      'https://media.playstation.com/is/image/SCEA/playstation-vue-hero-banner-desktop-01-us-22jan19?$native_nt$',
+    free_games_banner_1:
+      'https://th.bing.com/th/id/Refb29ff6809187d2cba9e340367d66f7?rik=16Td9AQg3LL7Cg&riu=http%3a%2f%2f2.bp.blogspot.com%2f-brXtJ3udkBk%2fUCnE_REJjsI%2fAAAAAAAACDU%2faDXC9brYZMo%2fs1600%2f46647-game-banner-full.png&ehk=4cYtiI0VSSFC8NdDjgGFDu97FcW%2fsYjKzsg4ILAB2W0%3d&risl=&pid=ImgRaw',
+    nsfw_banner:
+      'https://yesofcorsa.com/wp-content/uploads/2016/09/shokugeki_no_soma_wallpaper_download_3.png',
+    multiplayer_banner:
+      'https://playbingerepeat.files.wordpress.com/2018/05/fortnite-banner.png?w=1332&h=666&crop=1',
+    audio_control_thumbnail:
+      'http://www.extensions.in.th/amitiae/2013/prefs/images/sound_icon.png',
+    error_message_thumbnail:
+      'https://mir-s3-cdn-cf.behance.net/project_modules/disp/c9955d46715833.589222657aded.png',
+    ratelimit_thumbnail:
+      'https://th.bing.com/th/id/Rf7832050f813fd4298d1e088f0f3fa55?rik=7Xq2EbPkqJZkOA&riu=http%3a%2f%2fwikitravel.org%2fupload%2fen%2fb%2fb2%2fWarning-icon.png&ehk=PkXja2hMq1jutYB%2b0GFtPqmAz5kDFDNF46LFCMRThhs%3d&risl=&pid=ImgRaw',
+    error_thumbnail:
+      'https://upload.wikimedia.org/wikipedia/commons/thumb/2/27/Alert-Stop-Warning-Error_icon.svg/1024px-Alert-Stop-Warning-Error_icon.svg.png',
+    warning_thumbnail:
+      'https://th.bing.com/th/id/R56a2c39ff1eb81159883eb0199a23a55?rik=P0sSInV4jnLJ4A&riu=http%3a%2f%2ficons.iconarchive.com%2ficons%2fcreative-freedom%2fshimmer%2f256%2fWarning-icon.png&ehk=%2bzahp%2f2lHxKQmBVmJvWgliQGudrOs9%2ftdVwSnN5FQPM%3d&risl=&pid=ImgRaw',
+    streaming_banner:
+      'https://pa1.narvii.com/6771/d33918fa87ad0d84b7dc854dcbf6a8545c73f94d_hq.gif',
+  },
+  colors: {
+    play_role: '#7b00ff',
+    game_role: '#00ffff',
+    game_role_mentionable: '#00fffe',
+  },
 };
--- src/utils/ErrorTicketManager.js
+++ src/utils/ErrorTicketManager.js
@@ -1,22 +1,22 @@
 import { ErrorTicket } from '../types/Base.js';
 
 export default class ErrorTicketManager {
-	/**
-     * Constructs an object used to create error tickets.
-     * @param {String} location The location of this module.
-     */
-	constructor(location) {
-		this.location = location;
-	}
+  /**
+   * Constructs an object used to create error tickets.
+   * @param {string} location The location of this module.
+   */
+  constructor(location) {
+    this.location = location;
+  }
 
-	/**
-     * Creates an Error Ticket for the Error Manager.
-     * @param {String} method The name of the method that throwed the error.
-     * @param {String | Object} error The error message or object.
-     * @param {String} base The base method of the method that throwed the error.
-     * @returns {ErrorTicket} Error Ticket
-     */
-	create(method, error, base = '') {
-		return new ErrorTicket(this.location, method, error, base);
-	}
+  /**
+   * Creates an Error Ticket for the Error Manager.
+   * @param {string} method The name of the method that throwed the error.
+   * @param {string | Object} error The error message or object.
+   * @param {string} base The base method of the method that throwed the error.
+   * @returns {ErrorTicket} Error Ticket
+   */
+  create(method, error, base = '') {
+    return new ErrorTicket(this.location, method, error, base);
+  }
 }
