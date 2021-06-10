9901650e27a45f5903d634d58ad6b7925860f43c
Juruel Keanu Lorenzo <keanulorenzo32@gmail.com>
2021-06-10 19:41:15 +0800

    fix(MessageManager): TypeError: Cannot read property 'id' of null (#33)
    This fixes a bug where an error is thrown when a direct message is received.

--- src/managers/MessageManager.js
+++ src/managers/MessageManager.js
@@ -27,25 +27,18 @@ export default class MessageManager {
     this.client = client;
     this.queuer = new ProcessQueue(1000);
 
-    client.on('message', async message => {
-      try {
-        if (message.author.bot) return;
-        if (message.guild === null) {
-          await this.processIncomingDM(message);
-        }
+    client.on('message', message => {
+      if (message.author.bot) return;
+      if (message.guild === null) {
+        return this.processIncomingDM(message);
+      }
 
-        if (message.guild.id !== constants.cs.guild) return;
-        switch (message.channel.parentID) {
-          case constants.cs.channels.category.dm:
-            await this.processOutgoingDM(message);
-            break;
-
-          case constants.cs.channels.category.dc:
-            await this.processDirectChannels(message);
-            break;
-        }
-      } catch (error) {
-        this.client.error_manager.mark(ETM.create('message', error));
+      if (message.guild.id !== constants.cs.guild) return;
+      switch (message.channel.parentID) {
+        case constants.cs.channels.category.dm:
+          return this.processOutgoingDM(message);
+        case constants.cs.channels.category.dc:
+          return this.processDirectChannels(message);
       }
     });
   }
@@ -121,51 +114,57 @@ export default class MessageManager {
    * @param {Message} message The message received
    */
   async processIncomingDM(message) {
-    const this_member = this.client.member(message.author);
-    if (!this_member) return;
+    try {
+      const this_member = this.client.member(message.author);
+      if (!this_member) return;
 
-    /** @type {CategoryChannel} */
-    const dm_category = this.client.channel(constants.cs.channels.category.dm);
+      /** @type {CategoryChannel} */
+      const dm_category = this.client.channel(
+        constants.cs.channels.category.dm,
+      );
 
-    /** @type {TextChannel} */
-    const dm_channel =
-      dm_category.children.find(
-        c => c.isText() && contains(c.topic, this_member.toString()),
-      ) ??
-      (await this.client.channel_manager.create(this_member.displayName, {
-        guild: 'cs',
-        parent: dm_category,
-        topic:
-          `Direct message handler for ${this_member}. ` +
-          'You can reply to this user by sending a message to this channel.',
-      }));
+      /** @type {TextChannel} */
+      const dm_channel =
+        dm_category.children.find(
+          c => c.isText() && contains(c.topic, this_member.toString()),
+        ) ??
+        (await this.client.channel_manager.create(this_member.displayName, {
+          guild: 'cs',
+          parent: dm_category,
+          topic:
+            `Direct message handler for ${this_member}. ` +
+            'You can reply to this user by sending a message to this channel.',
+        }));
 
-    if (dm_channel.name !== this_member.displayName) {
-      await dm_channel.setName(this_member.displayName);
+      if (dm_channel.name !== this_member.displayName) {
+        await dm_channel.setName(this_member.displayName);
+      }
+
+      if (dm_channel.position !== 0) {
+        await dm_channel.setPosition(0);
+      }
+
+      // Max of 5 dm channels
+      if (dm_category.children.size > 5) {
+        dm_category.children
+          .filter(c => c.position > 4)
+          .forEach(c => {
+            // eslint-disable-next-line no-empty-function
+            this.client.channel_manager.delete(c).catch(() => {});
+          });
+      }
+
+      await this.sendToChannel(dm_channel, {
+        content: message.content?.length ? message.content : null,
+        files: message.attachments?.map(file => ({
+          attachment: file.attachment,
+          name: file.name,
+          data: file.data,
+        })),
+      });
+    } catch (error) {
+      this.client.error_manager.mark(ETM.create('processIncomingDM', error));
     }
-
-    if (dm_channel.position !== 0) {
-      await dm_channel.setPosition(0);
-    }
-
-    // Max of 5 dm channels
-    if (dm_category.children.size > 5) {
-      dm_category.children
-        .filter(c => c.position > 4)
-        .forEach(c => {
-          // eslint-disable-next-line no-empty-function
-          this.client.channel_manager.delete(c).catch(() => {});
-        });
-    }
-
-    await this.sendToChannel(dm_channel, {
-      content: message.content?.length ? message.content : null,
-      files: message.attachments?.map(file => ({
-        attachment: file.attachment,
-        name: file.name,
-        data: file.data,
-      })),
-    });
   }
 
   /**
@@ -173,28 +172,32 @@ export default class MessageManager {
    * @param {Message} message The message sent
    */
   async processOutgoingDM(message) {
-    /** @type {string} */
-    const topic = message.channel.topic;
-    const this_member = this.client.member(
-      topic.split(' ').find(w => this.client.member(w)),
-    );
-    if (!this_member) return;
-
-    const reply = await this.sendToUser(this_member, {
-      content: message.content?.length ? message.content : null,
-      files: message.attachments?.map(file => ({
-        attachment: file.attachment,
-        name: file.name,
-        data: file.data,
-      })),
-    });
-
-    if (reply) {
-      const emojis = this.client.emojis.cache;
-      await this.client.reaction_manager.add(
-        message,
-        emojis.find(e => e.name === 'checkgreen'),
+    try {
+      /** @type {string} */
+      const topic = message.channel.topic;
+      const this_member = this.client.member(
+        topic.split(' ').find(w => this.client.member(w)),
       );
+      if (!this_member) return;
+
+      const reply = await this.sendToUser(this_member, {
+        content: message.content?.length ? message.content : null,
+        files: message.attachments?.map(file => ({
+          attachment: file.attachment,
+          name: file.name,
+          data: file.data,
+        })),
+      });
+
+      if (reply) {
+        const emojis = this.client.emojis.cache;
+        await this.client.reaction_manager.add(
+          message,
+          emojis.find(e => e.name === 'checkgreen'),
+        );
+      }
+    } catch (error) {
+      this.client.error_manager.mark(ETM.create('processOutgoingDM', error));
     }
   }
 
@@ -203,36 +206,42 @@ export default class MessageManager {
    * @param {Message} message The message sent
    */
   async processDirectChannels(message) {
-    const message_data = {
-      content: message.content?.length ? message.content : null,
-      files: message.attachments?.map(file => ({
-        attachment: file.attachment,
-        name: file.name,
-        data: file.data,
-      })),
-    };
+    try {
+      const message_data = {
+        content: message.content?.length ? message.content : null,
+        files: message.attachments?.map(file => ({
+          attachment: file.attachment,
+          name: file.name,
+          data: file.data,
+        })),
+      };
 
-    let channel = '';
-    switch (message.channel.id) {
-      case constants.cs.channels.dc_announcements:
-        channel = constants.qg.channels.server.announcements;
-        break;
-      case constants.cs.channels.dc_rules:
-        channel = constants.qg.channels.server.rules;
-        break;
-      case constants.cs.channels.dc_roles:
-        channel = constants.qg.channels.server.roles;
-        break;
-      case constants.cs.channels.dc_guides:
-        channel = constants.qg.channels.server.guides;
-        break;
-      case constants.cs.channels.dc_suggestions:
-        channel = constants.qg.channels.server.suggestions;
-        break;
-    }
+      let channel = '';
+      switch (message.channel.id) {
+        case constants.cs.channels.dc_announcements:
+          channel = constants.qg.channels.server.announcements;
+          break;
+        case constants.cs.channels.dc_rules:
+          channel = constants.qg.channels.server.rules;
+          break;
+        case constants.cs.channels.dc_roles:
+          channel = constants.qg.channels.server.roles;
+          break;
+        case constants.cs.channels.dc_guides:
+          channel = constants.qg.channels.server.guides;
+          break;
+        case constants.cs.channels.dc_suggestions:
+          channel = constants.qg.channels.server.suggestions;
+          break;
+      }
 
-    if (channel) {
-      await this.client.message_manager.sendToChannel(channel, message_data);
+      if (channel) {
+        await this.client.message_manager.sendToChannel(channel, message_data);
+      }
+    } catch (error) {
+      this.client.error_manager.mark(
+        ETM.create('processDirectChannels', error),
+      );
     }
   }
 }
