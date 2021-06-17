40dc10e9eb90007a23201d1e0c68f3b2a8d9b76f
Juruel Keanu Lorenzo <39931559+JKLorenzo@users.noreply.github.com>
2021-06-18 01:19:16 +0800

    chore(Structures): cleanup Base.js

--- src/structures/Base.js
+++
@@ -1,6 +0,0 @@
-import Client from './Client.js';
-import ExtendedMessage from './ExtendedMessage.js';
-import MessageComponent from './MessageComponent.js';
-import SlashCommand from './SlashCommand.js';
-
-export { Client, ExtendedMessage, MessageComponent, SlashCommand };
