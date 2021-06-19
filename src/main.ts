05cdf0625b318d231161576ec3f205625c120856
Juruel Keanu Lorenzo <39931559+JKLorenzo@users.noreply.github.com>
2021-06-19 20:14:32 +0800

    feat(Main): utilize the new Client structure

--- src/main.ts
+++ src/main.ts
@@ -1,56 +1,14 @@
-import { Intents, Structures } from 'discord.js';
 import express from 'express';
-import { Client, ExtendedMessage } from './structures/Base';
-
-const {
-  DIRECT_MESSAGES,
-  GUILDS,
-  GUILD_BANS,
-  GUILD_EMOJIS,
-  GUILD_INVITES,
-  GUILD_MEMBERS,
-  GUILD_MESSAGES,
-  GUILD_PRESENCES,
-  GUILD_VOICE_STATES,
-} = Intents.FLAGS;
-
-Structures.extend('Message', () => ExtendedMessage);
+import Client from './structures/Client.js';
 
 const app = express();
-const client = new Client({
-  allowedMentions: {
-    parse: ['everyone', 'roles', 'users'],
-    repliedUser: true,
-  },
-  intents: [
-    DIRECT_MESSAGES,
-    GUILDS,
-    GUILD_BANS,
-    GUILD_EMOJIS,
-    GUILD_INVITES,
-    GUILD_MEMBERS,
-    GUILD_MESSAGES,
-    GUILD_PRESENCES,
-    GUILD_VOICE_STATES,
-  ],
-  partials: ['MESSAGE', 'CHANNEL'],
-  presence: {
-    activities: [
-      {
-        name: '/commands',
-        type: 'LISTENING',
-      },
-    ],
-    status: 'online',
-    afk: false,
-  },
-});
+const client = new Client();
 
 app.get('/', (req, res) => {
   res.send(client?.ws?.ping ? 'online' : 'offline');
 });
 
-app.listen(process.env.PORT || 3000, () => {
+app.listen(process.env.PORT ?? 3000, () => {
   console.log('Server is running...');
   client.login(process.env.BOT_TOKEN);
 });
