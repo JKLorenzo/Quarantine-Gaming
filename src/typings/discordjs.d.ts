8acc0e5525c379f72bc29526644fd11446aa9730
Juruel Keanu Lorenzo <39931559+JKLorenzo@users.noreply.github.com>
2021-06-20 19:57:07 +0800

    types(DiscordJs): add GuildMember interface

--- src/typings/discordjs.d.ts
+++ src/typings/discordjs.d.ts
@@ -1,17 +1,16 @@
 import Channel_Manager from '../managers/ChannelManager.js';
+import Database_Manager from '../managers/DatabaseManager.js';
 import Error_Manager from '../managers/ErrorManager.js';
 import Message_Manager from '../managers/MessageManager.js';
 import Reaction_Manager from '../managers/ReactionManager.js';
 import Role_Manager from '../managers/RoleManager.js';
 import Speech_Manager from '../managers/SpeechManager.js';
+import { PartialRole } from '../structures/Interfaces.js';
 
 declare module 'discord.js' {
-  interface Message {
-    delete(options?: { timeout: number }): Promise<Message>;
-  }
-
   interface Client {
     readonly channel_manager: Channel_Manager;
+    readonly database_manager: Database_Manager;
     readonly error_manager: Error_Manager;
     readonly message_manager: Message_Manager;
     readonly reaction_manager: Reaction_Manager;
@@ -28,5 +27,20 @@ declare module 'discord.js' {
       channel: GuildChannelResolvable,
       message: MessageResolvable,
     ): Message | null;
+  }
+
+  interface Message {
+    delete(options?: { timeout: number }): Promise<Message>;
+  }
+
+  interface GuildMember {
+    register(inviter: GuildMember, moderator: GuildMember): Promise<void>;
+
+    fetchInviter(): Promise<GuildMember | null>;
+    fetchModerator(): Promise<GuildMember | null>;
+
+    fetchExpiredGameRoles(): Promise<PartialRole[]>;
+    updateGameRole(role: Role): Promise<void>;
+    deleteGameRole(id: `${bigint}`): Promise<void>;
   }
 }
