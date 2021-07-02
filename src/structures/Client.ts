fb9107c02c00e52af71c9f31e671efd18c7f2562
Juruel Keanu Lorenzo <39931559+JKLorenzo@users.noreply.github.com>
2021-07-02 14:23:10 +0800

    feat(Client): remove helpers

--- src/structures/Client.ts
+++ src/structures/Client.ts
@@ -1,17 +1,4 @@
-import {
-  Client,
-  Guild,
-  GuildChannel,
-  GuildChannelResolvable,
-  GuildMember,
-  Intents,
-  Message,
-  MessageResolvable,
-  Role,
-  RoleResolvable,
-  Structures,
-  UserResolvable,
-} from 'discord.js';
+import { Client, Guild, Intents, Structures } from 'discord.js';
 import ExtendedMember from './ExtendedMember.js';
 import ExtendedMessage from './ExtendedMessage.js';
 import ChannelManager from '../managers/ChannelManager.js';
@@ -21,7 +8,6 @@ import ReactionManager from '../managers/ReactionManager.js';
 import RoleManager from '../managers/RoleManager.js';
 import SpeechManager from '../managers/SpeechManager.js';
 import constants from '../utils/Constants.js';
-import { parseMention } from '../utils/Functions.js';
 
 Structures.extend('GuildMember', () => ExtendedMember);
 Structures.extend('Message', () => ExtendedMessage);
@@ -78,42 +64,5 @@ export default class extends Client {
 
   get cs(): Guild {
     return this.guilds.cache.get(`${constants.cs.guild}`)!;
-  }
-
-  channel(channel: GuildChannelResolvable): GuildChannel | undefined {
-    const parsed_channel = parseMention(channel);
-    return (
-      this.qg.channels.resolve(parsed_channel) ??
-      this.cs.channels.resolve(parsed_channel) ??
-      undefined
-    );
-  }
-
-  member(user: UserResolvable): GuildMember | undefined {
-    const parsed_user = parseMention(user);
-    return (
-      this.qg.members.resolve(parsed_user) ??
-      this.cs.members.resolve(parsed_user) ??
-      undefined
-    );
-  }
-
-  role(role: RoleResolvable): Role | undefined {
-    const parsed_role = parseMention(role);
-    return (
-      this.qg.roles.resolve(parsed_role) ??
-      this.cs.roles.resolve(parsed_role) ??
-      undefined
-    );
-  }
-
-  message(
-    channel: GuildChannelResolvable,
-    message: MessageResolvable,
-  ): Message | undefined {
-    const this_channel = this.channel(channel);
-    if (this_channel?.isText()) {
-      return this_channel.messages.resolve(message) ?? undefined;
-    }
   }
 }
